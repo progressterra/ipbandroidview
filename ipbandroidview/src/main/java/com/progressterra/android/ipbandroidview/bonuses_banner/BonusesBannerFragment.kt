65f60d88323368eca360f4faac6de121c585db6e
Ilya Stupak <>
2021-05-21 12:35:52 +0300

    Поправлена логика в фрагмете и модели

--- ipbandroidview/src/main/java/com/progressterra/android/ipbandroidview/bonuses_banner/BonusesBannerFragment.kt
+++ ipbandroidview/src/main/java/com/progressterra/android/ipbandroidview/bonuses_banner/BonusesBannerFragment.kt
@@ -1,6 +1,7 @@
 package com.progressterra.android.ipbandroidview.bonuses_banner
 
 import android.graphics.Color
+import android.graphics.PorterDuff
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.util.TypedValue.COMPLEX_UNIT_SP
@@ -13,7 +14,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.progressterra.ipbandroidview.R
 
-
 class BonusesBannerFragment : Fragment() {
 
     private lateinit var bonusesBannerViewModel: BonusesBannerViewModel
@@ -23,7 +23,6 @@ class BonusesBannerFragment : Fragment() {
     private lateinit var nextBtn: ImageView
     private lateinit var backgroundIv: ImageView
     private lateinit var numberOfBonusesTv: TextView
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,18 +35,18 @@ class BonusesBannerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initView(view)
+        applyFragmentArguments()
         setupViewModel()
     }
 
     private fun setupViewModel() {
         bonusesBannerViewModel = ViewModelProvider(this).get(BonusesBannerViewModel::class.java)
         bonusesBannerViewModel.bonusesInfo.observe(this) {
-            it.data.currentQuantity
-            it.data.dateBurning
-            it.data.forBurningQuantity
-            it.data.typeBonusName
+            numberOfBonusesTv.text = getString(R.string.bonuses_count, it.currentQuantity.toString())
+            bonusExpirationDateTv.text = it.dateBurning
+            bonusExpirationNumberTv.text = it.forBurningQuantity.toString()
         }
-        bonusesBannerViewModel.bonusesCountIsZero.observe(this) {
+        bonusesBannerViewModel.bonusesCountIsNonZero.observe(this) {
             showAllBonusesInfo(!it)
         }
     }
@@ -77,36 +76,119 @@ class BonusesBannerFragment : Fragment() {
         nextBtn.setOnClickListener(listener)
     }
 
-    fun setNextButtonColor(color: String) {
+    private fun applyFragmentArguments() {
+        arguments?.apply {
+            getString(NEXT_BUTTON_COLOR_ARG)?.let {
+                setNextButtonColor(it)
+            }
+
+            getString(BASE_TEXT_COLOR_ARG)?.let {
+                setBaseTextColor(it)
+            }
+
+            getString(HEADER_TEXT_COLOR_ARG)?.let {
+                setHeaderColor(it)
+            }
+
+            val firstBackgroundColor = getString(FIRST_BACKGROUND_COLOR_ARG)
+            val secondBackgroundColor = getString(SECOND_BACKGROUND_COLOR_ARG)
+            setBackgroundColor(firstBackgroundColor, secondBackgroundColor)
+            getFloat(BASE_TEXT_SIZE_ARG).let {
+                if (it != 0f) {
+                    setBaseTextSize(it)
+                }
+            }
+            getFloat(HEADER_TEXT_SIZE_ARG).let {
+                if (it != 0f) {
+                    setHeaderTextSize(it)
+                }
+            }
+        }
+    }
+
+    private fun setNextButtonColor(color: String) {
         nextBtn.setColorFilter(
             Color.parseColor(color),
-            android.graphics.PorterDuff.Mode.SRC_IN
+            PorterDuff.Mode.SRC_IN
         )
     }
 
-    fun setBackgroundColor(vararg color: String) {
-        val parsedColor = color.map { Color.parseColor(it) }
+    private fun setBackgroundColor(vararg colors: String?) {
+        val colorList: List<String?> = colors.toList().filterNotNull()
+
+        if (colorList.isNullOrEmpty()) {
+            return
+        }
+        if (colorList.size == 1) {
+            val simpleDrawable = GradientDrawable()
+            simpleDrawable.setColor(Color.parseColor(colorList.first()))
+            backgroundIv.setImageDrawable(simpleDrawable)
+            return
+        }
+        val parsedColor = colorList.map { Color.parseColor(it) }
         val gradientDrawable =
             GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, parsedColor.toIntArray())
-        gradientDrawable.cornerRadius = 20f
         backgroundIv.setImageDrawable(gradientDrawable)
     }
 
-    fun setBaseTextSize(textSize: Float) {
+    private fun setBaseTextSize(textSize: Float) {
         bonusExpirationDateTv.setTextSize(COMPLEX_UNIT_SP, textSize)
         bonusExpirationNumberTv.setTextSize(COMPLEX_UNIT_SP, textSize)
     }
 
-    fun setHeaderTextSize(textSize: Float) {
+    private fun setHeaderTextSize(textSize: Float) {
         numberOfBonusesTv.setTextSize(COMPLEX_UNIT_SP, textSize)
     }
 
-    fun setBaseTextColor(color: String) {
+    private fun setBaseTextColor(color: String) {
         bonusExpirationDateTv.setTextColor(Color.parseColor(color))
         bonusExpirationNumberTv.setTextColor(Color.parseColor(color))
     }
 
-    fun setHeaderColor(color: String) {
+    private fun setHeaderColor(color: String) {
         numberOfBonusesTv.setTextColor(Color.parseColor(color))
+    }
+
+    fun updateBonusesInfo() {
+        bonusesBannerViewModel.updateBonusesInfo()
+    }
+
+    companion object {
+
+        const val NEXT_BUTTON_COLOR_ARG = "next_button_color"
+        const val FIRST_BACKGROUND_COLOR_ARG = "first_background_color"
+        const val SECOND_BACKGROUND_COLOR_ARG = "second_background_color"
+        const val BASE_TEXT_SIZE_ARG = "base_text_size"
+        const val HEADER_TEXT_SIZE_ARG = "header_text_size"
+        const val BASE_TEXT_COLOR_ARG = "base_text_color"
+        const val HEADER_TEXT_COLOR_ARG = "header_text_color"
+
+        fun newInstance(
+            nextButtonColor: String? = null,
+            firstBackgroundColor: String? = null,
+            secondBackgroundColor: String? = null,
+            baseTextSize: Float? = null,
+            headerTextSize: Float? = null,
+            baseTextColor: String? = null,
+            headerTextColor: String? = null,
+        ): BonusesBannerFragment {
+            return BonusesBannerFragment().apply {
+                val args = Bundle()
+                args.apply {
+                    putString(NEXT_BUTTON_COLOR_ARG, nextButtonColor)
+                    putString(FIRST_BACKGROUND_COLOR_ARG, firstBackgroundColor)
+                    putString(SECOND_BACKGROUND_COLOR_ARG, secondBackgroundColor)
+                    putString(BASE_TEXT_COLOR_ARG, baseTextColor)
+                    putString(HEADER_TEXT_COLOR_ARG, headerTextColor)
+                    baseTextSize?.let {
+                        putFloat(BASE_TEXT_SIZE_ARG, it)
+                    }
+                    headerTextSize?.let {
+                        putFloat(HEADER_TEXT_SIZE_ARG, it)
+                    }
+                    arguments = args
+                }
+            }
+        }
     }
 }
--- ipbandroidview/src/main/java/com/progressterra/android/ipbandroidview/bonuses_banner/BonusesBannerViewModel.kt
+++ ipbandroidview/src/main/java/com/progressterra/android/ipbandroidview/bonuses_banner/BonusesBannerViewModel.kt
@@ -2,16 +2,18 @@ package com.progressterra.android.ipbandroidview.bonuses_banner
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.progressterra.ipbandroidapi.interactors.BonusesBannerInteractor
-import com.progressterra.ipbandroidapi.repository.models.GeneralInfoResponse
+import asd.android.networkrepository.GeneralInfoResponseConverter
+import com.progressterra.ipbandroidapi.bonuses_repository.BonusesInfoRepositoryImpl
+import com.progressterra.ipbandroidapi.repository.models.base.GlobalResponseStatus
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class BonusesBannerViewModel : ViewModel() {
 
-    val bonusesInfo = MutableLiveData<GeneralInfoResponse>()
-    val bonusesCountIsZero = MutableLiveData(false)
+    val bonusesInfo = MutableLiveData<BonusesInfo>()
+    val bonusesCountIsNonZero = MutableLiveData(false)
+    val repository = BonusesInfoRepositoryImpl()
 
     init {
         updateBonusesInfo()
@@ -19,15 +21,32 @@ class BonusesBannerViewModel : ViewModel() {
 
     fun updateBonusesInfo() {
         CoroutineScope(Job()).launch {
-            val bonusesInfoResponse = BonusesBannerInteractor().getBonusesInfo().body()
-            bonusesInfoResponse?.let {
-                if (it.data.currentQuantity == 0.0) {
-                    bonusesCountIsZero.postValue(false)
-                } else {
-                    bonusesCountIsZero.postValue(true)
-                }
+            getAccessToken()?.let {
+                getGeneralBonusesInfo(it)
             }
         }
+    }
+
+    private suspend fun getGeneralBonusesInfo(accessToken: String) {
+        repository.getBonusesInfo(accessToken).let { generalInfoResponse ->
+            if (generalInfoResponse.globalResponseStatus == GlobalResponseStatus.SUCCESS) {
+                with(bonusesCountIsNonZero) { postValue(generalInfoResponse.responseBody?.data?.forBurningQuantity != 0.0) }
+                bonusesInfo.postValue(
+                    GeneralInfoResponseConverter.convert(
+                        generalInfoResponse.responseBody!!.data
+                    )
+                )
+            }
+        }
+    }
+
+    private suspend fun getAccessToken(): String? {
+        repository.getAccessToken().let {
+            if (it.globalResponseStatus == GlobalResponseStatus.SUCCESS) {
+                return it.responseBody?.accessToken
+            }
+        }
+        return null
     }
 
 }
