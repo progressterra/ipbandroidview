e8d1200939f50ded28df8339218129904f127a1c
ToMaTo_0 <47805315+IncredibleHohol@users.noreply.github.com>
2021-08-19 14:47:27 +0300

    fixes for prev changes

--- ipbandroidview/src/main/java/com/progressterra/ipbandroidview/ui/login/settings/PhoneNumberSettings.kt
+++ ipbandroidview/src/main/java/com/progressterra/ipbandroidview/ui/login/settings/PhoneNumberSettings.kt
@@ -2,7 +2,7 @@ package com.progressterra.ipbandroidview.ui.login.settings
 
 import android.os.Parcelable
 import com.progressterra.ipbandroidview.ui.login.country.enums.Country
-import com.progressterra.ipbandroidview.utils.DefaultArgsValues
+import com.progressterra.ipbandroidview.utils.DEFAULT_RES
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -10,5 +10,5 @@ data class PhoneNumberSettings(
     val defaultCountry: String = Country.RUSSIA.name,
     val agreementEnabled: Boolean = false,
     val footerEnabled: Boolean = false,
-    val footerImageId: Int = DefaultArgsValues.DEFAULT_RES
+    val footerImageId: Int = DEFAULT_RES
 ) : Parcelable
