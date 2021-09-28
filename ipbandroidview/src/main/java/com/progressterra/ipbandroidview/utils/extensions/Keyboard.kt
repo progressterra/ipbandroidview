4035e5fb4f5bc801112d86ae37b7bd76f28e931e
ToMaTo_0 <47805315+IncredibleHohol@users.noreply.github.com>
2021-09-28 11:54:44 +0300

    hide keyboardWithActivity

--- ipbandroidview/src/main/java/com/progressterra/ipbandroidview/utils/extensions/Keyboard.kt
+++ ipbandroidview/src/main/java/com/progressterra/ipbandroidview/utils/extensions/Keyboard.kt
@@ -1,17 +1,18 @@
 package com.progressterra.ipbandroidview.utils.extensions
 
+import android.app.Activity
 import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
-fun showKeyboard(context: Context, view: View) {
+fun showKeyboard(activity: Activity, view: View) {
     val imm =
-        context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
 }
 
-fun hideKeyboard(context: Context, view: View) {
+fun hideKeyboard(activity: Activity, view: View) {
     val imm =
-        context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.showSoftInput(view, InputMethodManager.HIDE_IMPLICIT_ONLY)
 }
