afefeda036efa0c8158e8531c70778167af37ad5
ToMaTo_0 <47805315+IncredibleHohol@users.noreply.github.com>
2021-09-28 12:06:10 +0300

    clear\request focus to view

--- ipbandroidview/src/main/java/com/progressterra/ipbandroidview/utils/extensions/Keyboard.kt
+++ ipbandroidview/src/main/java/com/progressterra/ipbandroidview/utils/extensions/Keyboard.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 fun showKeyboard(activity: Activity, view: View) {
+    view.requestFocus()
     val imm =
         activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
@@ -17,4 +18,5 @@ fun hideKeyboard(activity: Activity, view: View) {
 //    imm.showSoftInput(view, InputMethodManager.HIDE_IMPLICIT_ONLY)
     val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
     imm?.hideSoftInputFromWindow(view.windowToken, 0)
+    view.clearFocus()
 }
