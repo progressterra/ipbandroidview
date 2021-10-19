c76739b0d1c3e2605620383d36170a51961a66da
Ilya Stupak <>
2021-10-19 18:37:12 +0300

    Правки замечаний в флоу приглашения друзей

--- ipbandroidview/src/main/java/com/progressterra/ipbandroidview/ui/user_inviting/InviteUserViewModel.kt
+++ ipbandroidview/src/main/java/com/progressterra/ipbandroidview/ui/user_inviting/InviteUserViewModel.kt
@@ -56,6 +56,9 @@ class InviteUserViewModel : BaseBindingViewModel() {
         }
     }
 
+    fun clearSearchQuery() {
+        search.value = ""
+    }
 
     fun selectContact(contactUi: ContactUi) {
         mContacts[mContacts.indexOf(contactUi)] = contactUi
