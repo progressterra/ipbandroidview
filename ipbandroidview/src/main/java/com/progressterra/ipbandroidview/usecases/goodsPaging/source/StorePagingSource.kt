e28cae88be40d29774c68fcb858c3699e1b8103e
Ilya Stupak <>
2021-10-29 11:16:18 +0300

    Фикс пагинации

--- ipbandroidview/src/main/java/com/progressterra/ipbandroidview/usecases/goodsPaging/source/StorePagingSource.kt
+++ ipbandroidview/src/main/java/com/progressterra/ipbandroidview/usecases/goodsPaging/source/StorePagingSource.kt
@@ -15,7 +15,9 @@ internal class StorePagingSource(
     private val idCategory: String
 ) : PagingSource<Int, RGGoodsInventoryExt>() {
     override fun getRefreshKey(state: PagingState<Int, RGGoodsInventoryExt>): Int {
-        return DEF_PAGE
+        val anchorPos = state.anchorPosition ?: return DEF_PAGE
+        val page = state.closestPageToPosition(anchorPos) ?: return DEF_PAGE
+        return page.nextKey?.minus(1) ?: page.prevKey?.plus(1) ?: DEF_PAGE
     }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, RGGoodsInventoryExt> {
