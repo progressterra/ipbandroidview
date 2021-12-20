ff00a181529a17dfce3b8a93579360a3c93878da
kmem1 <konstantinovich.aleks@mail.ru>
2021-12-20 17:38:40 +0300

    Add clearUser in UserDataLocal

--- ipbandroidview/src/main/java/com/progressterra/ipbandroidview/data/prefs/UserDataLocal.kt
+++ ipbandroidview/src/main/java/com/progressterra/ipbandroidview/data/prefs/UserDataLocal.kt
@@ -5,4 +5,9 @@ import com.chibatching.kotpref.KotprefModel
 object UserDataLocal : KotprefModel() {
     var city by stringPref("")
     var avatarUrl by stringPref("")
+
+    fun clearUser() {
+        city = ""
+        avatarUrl = ""
+    }
 }
