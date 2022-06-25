3fbfea64767ffe28cd12dd64576e9e9548c95f21
velvet <berhram@protonmail.com>
2022-06-25 14:04:56 +0300

    fix: input edittexts

--- ipbandroidview/src/main/java/com/progressterra/ipbandroidview/ui/login/personal/PersonalFragment.kt
+++ ipbandroidview/src/main/java/com/progressterra/ipbandroidview/ui/login/personal/PersonalFragment.kt
@@ -199,7 +199,7 @@ class PersonalFragment : BaseFragment() {
         dialog.updateDate(DEFAULT_YEAR, DEFAULT_MONTH, DEFAULT_DAY)
         dialog.datePicker.maxDate = System.currentTimeMillis()
 
-        binding.personalData.etBirthDay.setOnClickListener {
+        binding.personalData.vBirthDay.setOnClickListener {
             Log.d("BIRTH", "Dialog show")
             dialog.show()
         }
