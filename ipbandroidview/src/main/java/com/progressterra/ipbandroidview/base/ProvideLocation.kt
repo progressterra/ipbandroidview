aca5dfd443835ccbfc86be240386ae06c90ad505
velvet <berhram@protonmail.com>
2022-09-28 22:20:09 +0300

    feat: location obtaining

--- ipbandroidview/src/main/java/com/progressterra/ipbandroidview/base/ProvideLocation.kt
+++ ipbandroidview/src/main/java/com/progressterra/ipbandroidview/base/ProvideLocation.kt
@@ -1,9 +1,22 @@
 package com.progressterra.ipbandroidview.base
 
 import android.location.Location
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.Tasks
 
 
 interface ProvideLocation {
 
-    fun location(): Location
+    suspend fun location(): Location
+
+    class Base(private val fusedLocationClient: FusedLocationProviderClient) : ProvideLocation {
+
+        override suspend fun location(): Location = Tasks.await(
+            fusedLocationClient.getCurrentLocation(
+                Priority.PRIORITY_HIGH_ACCURACY,
+                null
+            )
+        )
+    }
 }
