379630d3fc676067aa053883a47a3007d35c9cbd
velvet <berhram@protonmail.com>
2022-10-03 23:56:04 +0300

    feat: audit cards

---
+++ ipbandroidview/src/main/java/com/progressterra/ipbandroidview/composable/AuditCard.kt
@@ -0,0 +1,110 @@
+package com.progressterra.ipbandroidview.composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.progressterra.ipbandroidview.R
+import com.progressterra.ipbandroidview.theme.AppTheme
+
+@Composable
+fun AuditCard(
+    modifier: Modifier = Modifier,
+    cornerRadius: Dp = 12.dp,
+    padding: Dp = 12.dp,
+    verticalGap: Dp = 2.dp,
+    horizontalGap: Dp = 8.dp,
+    text: String = "",
+    address: String = "",
+    percentage: Int = 0,
+    successPercentage: Int = 50,
+    done: Boolean = false
+) {
+    Row(
+        modifier = modifier
+            .clip(RoundedCornerShape(cornerRadius))
+            .background(AppTheme.colors.surfaces)
+            .padding(padding),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(modifier = Modifier.weight(1f)) {
+            Text(
+                text = text,
+                color = AppTheme.colors.black,
+                style = AppTheme.typography.text,
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            Spacer(modifier = Modifier.size(verticalGap))
+            Text(
+                text = address,
+                color = AppTheme.colors.gray2,
+                style = AppTheme.typography.tertiaryText,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+        if (!done) {
+            Spacer(modifier = Modifier.size(horizontalGap))
+            Percentage(
+                percentage = percentage,
+                successPercentage = successPercentage
+            )
+            Icon(
+                modifier = modifier.size(16.dp),
+                painter = painterResource(id = R.drawable.ic_forward),
+                contentDescription = stringResource(
+                    id = R.string.forward
+                ),
+                tint = AppTheme.colors.gray2
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun AuditCardPreviewEmpty() {
+    AppTheme {
+        AuditCard(
+            text = "Ирина проводит аудит: «Наличие необходимого минимума продовольственных запасов на складе»",
+            address = "пл Дружбы народов, 45, «Кофемания»",
+            percentage = 15
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun AuditCardPreviewZero() {
+    AppTheme {
+        AuditCard(
+            text = "Ирина проводит аудит: «Наличие необходимого минимума продовольственных запасов на складе»",
+            address = "пл Дружбы народов, 45, «Кофемания»"
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun AuditCardPreviewFull() {
+    AppTheme {
+        AuditCard(
+            text = "Ирина проводит аудит: «Наличие необходимого минимума продовольственных запасов на складе»",
+            address = "пл Дружбы народов, 45, «Кофемания»",
+            percentage = 89
+        )
+    }
+}
---
+++ ipbandroidview/src/main/java/com/progressterra/ipbandroidview/composable/Percentage.kt
@@ -0,0 +1,53 @@
+package com.progressterra.ipbandroidview.composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.progressterra.ipbandroidview.theme.AppTheme
+
+@Composable
+fun Percentage(
+    modifier: Modifier = Modifier,
+    percentage: Int = 0,
+    successPercentage: Int = 50,
+    cornerRadius: Dp = 100.dp,
+    verticalPadding: Dp = 2.dp,
+    horizontalPadding: Dp = 4.dp
+) {
+    Row(
+        modifier = Modifier
+            .clip(RoundedCornerShape(cornerRadius))
+            .background(if (percentage >= successPercentage) AppTheme.colors.primary else AppTheme.colors.error)
+            .padding(horizontal = horizontalPadding, vertical = verticalPadding)
+    ) {
+        Text(
+            text = "$percentage%",
+            color = AppTheme.colors.surfaces,
+            style = AppTheme.typography.actionBarLabels
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PercentagePreviewFailed() {
+    AppTheme {
+        Percentage(percentage = 15)
+    }
+}
+
+@Preview
+@Composable
+private fun PercentagePreviewSuccessful() {
+    AppTheme {
+        Percentage(percentage = 55)
+    }
+}
