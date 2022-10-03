1b0821903f5459836b4b17b95e9e20897dfdd484
velvet <berhram@protonmail.com>
2022-10-03 12:38:22 +0300

    debug: logging

--- ipbandroidview/src/main/java/com/progressterra/ipbandroidview/ui/signin/SignInViewModel.kt
+++ ipbandroidview/src/main/java/com/progressterra/ipbandroidview/ui/signin/SignInViewModel.kt
@@ -1,9 +1,13 @@
 package com.progressterra.ipbandroidview.ui.signin
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.progressterra.ipbandroidapi.user.UserData
 import com.progressterra.ipbandroidview.R
 import com.progressterra.ipbandroidview.domain.StartVerificationChannelUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 import org.orbitmvi.orbit.Container
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
@@ -18,11 +22,13 @@ class SignInViewModel(
     override val container: Container<SignInState, SignInEffect> = container(SignInState())
 
     override fun onNext() = intent {
-        if (startVerificationChannelUseCase.start(state.phoneNumber)) {
-            UserData.phone = state.phoneNumber
-            postSideEffect(SignInEffect.Next)
-        } else
-            postSideEffect(SignInEffect.Toast(R.string.wrong_phone))
+        withContext(Dispatchers.IO) {
+            if (startVerificationChannelUseCase.start(state.phoneNumber)) {
+                UserData.phone = state.phoneNumber
+                postSideEffect(SignInEffect.Next)
+            } else
+                postSideEffect(SignInEffect.Toast(R.string.wrong_phone))
+        }
     }
 
     override fun onSkip() = intent {
@@ -33,9 +39,20 @@ class SignInViewModel(
         postSideEffect(SignInEffect.Back)
     }
 
-    override fun onPhoneNumber(phoneNumber: String) = intent {
-        reduce {
-            state.copy(phoneNumber = phoneNumber)
+    override fun onPhoneNumber(phoneNumber: String) {
+        Log.d("INTENT", "onPhoneNumber start: ${Thread.currentThread().name}")
+        intent {
+            Log.d("INTENT", "onPhoneNumber in intent 1: ${Thread.currentThread().name}")
+            reduce {
+                state.copy(phoneNumber = phoneNumber)
+            }
         }
+        intent {
+            withContext(Dispatchers.IO) {
+                Log.d("INTENT", "onPhoneNumber in intent 2: ${Thread.currentThread().name}")
+                delay(5000)
+            }
+        }
+        Log.d("INTENT", "onPhoneNumber finished: ${Thread.currentThread().name}")
     }
 }
