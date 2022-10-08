97f8da05d50386c5d230b46d6931a88ecb456e24
velvet <berhram@protonmail.com>
2022-10-08 07:44:26 +0300

    fix: organizations fix

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/ui/organizationaudits/OrganizationInfo.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/ui/organizationaudits/OrganizationInfo.kt
@@ -1,8 +1,12 @@
 package com.progressterra.ipbandroidview.ui.organizationaudits
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class OrganizationInfo(
     val id: String,
     val imageUrl: String,
     val name: String,
     val address: String
-)
+) : Parcelable
