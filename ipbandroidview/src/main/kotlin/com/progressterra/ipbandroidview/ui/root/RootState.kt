12890e6d797bca0922da4b40243353bab3944de2
velvet <berhram@protonmail.com>
2022-10-08 03:21:27 +0300

    fix: composable annotation

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/ui/root/RootState.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/ui/root/RootState.kt
@@ -1,33 +1,34 @@
 package com.progressterra.ipbandroidview.ui.root
 
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.runtime.Composable
 import com.progressterra.ipbandroidview.R
 import com.progressterra.ipbandroidview.composable.bottomnav.BottomMenuItem
 
 data class RootState(
     val onBack: (() -> Unit)? = null,
     val title: String? = "",
-    val actions: (RowScope.() -> Unit)? = null,
+    val actions: (@Composable RowScope.() -> Unit)? = null,
     val bottomBarVisibility: Boolean = false,
 ) {
 
     companion object Item {
 
-        fun buildAudits(active: Boolean, count: Int = 0) = BottomMenuItem(
+        fun buildAudits(count: Int = 0) = BottomMenuItem(
             id = "audits",
             titleId = R.string.audits,
             count = count,
             iconId = R.drawable.ic_audits
         )
 
-        fun buildOrganizations(active: Boolean, count: Int = 0) = BottomMenuItem(
+        fun buildOrganizations(count: Int = 0) = BottomMenuItem(
             id = "organizations",
             titleId = R.string.organizations,
             count = count,
             iconId = R.drawable.ic_organization
         )
 
-        fun buildProfile(active: Boolean, count: Int = 0) = BottomMenuItem(
+        fun buildProfile(count: Int = 0) = BottomMenuItem(
             id = "profile",
             titleId = R.string.profile,
             count = count,
