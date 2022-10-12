792a22632281b98681413af9c4dd3249be540fb4
velvet <berhram@protonmail.com>
2022-10-12 19:03:03 +0300

    fix: location exception handling

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/data/ProvideLocation.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/data/ProvideLocation.kt
@@ -8,15 +8,21 @@ import com.google.android.gms.tasks.Tasks
 
 interface ProvideLocation {
 
-    suspend fun location(): Location
+    suspend fun location(): Result<Location>
 
     class Base(private val fusedLocationClient: FusedLocationProviderClient) : ProvideLocation {
 
-        override suspend fun location(): Location = Tasks.await(
-            fusedLocationClient.getCurrentLocation(
-                Priority.PRIORITY_HIGH_ACCURACY,
-                null
+        override suspend fun location(): Result<Location> = try {
+            Result.success(
+                Tasks.await(
+                    fusedLocationClient.getCurrentLocation(
+                        Priority.PRIORITY_HIGH_ACCURACY,
+                        null
+                    )
+                )
             )
-        )
+        } catch (exception: Exception) {
+            Result.failure(exception)
+        }
     }
 }
