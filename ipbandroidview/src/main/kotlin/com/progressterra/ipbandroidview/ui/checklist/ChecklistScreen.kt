05f82fe1a5ec81573999b5452e3224f614aa4b28
velvet <berhram@protonmail.com>
2022-10-20 20:39:44 +0300

    fix: ui

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/ui/checklist/ChecklistScreen.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/ui/checklist/ChecklistScreen.kt
@@ -1,6 +1,5 @@
 package com.progressterra.ipbandroidview.ui.checklist
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -32,7 +32,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -53,6 +54,7 @@ import com.progressterra.ipbandroidview.composable.yesno.YesNoButton
 import com.progressterra.ipbandroidview.theme.AppTheme
 import kotlinx.coroutines.launch
 
+@Suppress("unused")
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun ChecklistScreen(state: ChecklistState, interactor: ChecklistInteractor) {
@@ -75,7 +77,7 @@ fun ChecklistScreen(state: ChecklistState, interactor: ChecklistInteractor) {
                 }
             })
             StateBox(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth().heightIn(min = 300.dp),
                 state = state.screenState,
                 onRefresh = { interactor.refresh() }) {
                 if (state.currentCheck != null && state.currentCheckDetails != null) {
@@ -217,13 +219,13 @@ fun ChecklistScreen(state: ChecklistState, interactor: ChecklistInteractor) {
                     item { Spacer(modifier = Modifier.size(spacerSize + 8.dp)) }
                 }
                 if (!state.checklist.done) {
+                    val density = LocalDensity.current
                     BottomHolder(
                         Modifier
                             .fillMaxWidth()
                             .align(Alignment.BottomCenter)
-                            .onSizeChanged {
-                                spacerSize = it.height.dp
-                                Log.d("SCREEN", "Height: $spacerSize")
+                            .onGloballyPositioned {
+                                spacerSize = with(density) { it.size.height.toDp() }
                             }
                     ) {
                         Row {
--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/ui/documents/DocumentsScreen.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/ui/documents/DocumentsScreen.kt
@@ -17,15 +17,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import com.progressterra.ipbandroidview.R
-import com.progressterra.ipbandroidview.composable.StateBox
 import com.progressterra.ipbandroidview.composable.CategoryDivider
 import com.progressterra.ipbandroidview.composable.DocumentCard
+import com.progressterra.ipbandroidview.composable.StateBox
 import com.progressterra.ipbandroidview.composable.ThemedButton
 import com.progressterra.ipbandroidview.composable.ThemedTopAppBar
 import com.progressterra.ipbandroidview.composable.stats.ChecklistStats
@@ -88,13 +89,16 @@ fun DocumentsScreen(state: DocumentsState, interactor: DocumentsInteractor) {
                     Spacer(modifier = Modifier.size(spacerSize + 32.dp))
                 }
             }
+            val density = LocalDensity.current
             ThemedButton(
                 modifier = Modifier
                     .zIndex(1f)
                     .align(Alignment.BottomCenter)
                     .padding(start = 24.dp, end = 24.dp, bottom = 24.dp)
                     .fillMaxWidth()
-                    .onSizeChanged { spacerSize = it.height.dp },
+                    .onGloballyPositioned {
+                        spacerSize = with(density) { it.size.height.toDp() }
+                    },
                 onClick = { interactor.onAudit() },
                 text = stringResource(id = R.string.create_audit)
             )
