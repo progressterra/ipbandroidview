c0ec5097d16ab39aec2117975983573072ef1f7a
velvet <berhram@protonmail.com>
2022-10-20 09:53:04 +0300

    fix: ui fix

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/ui/checklist/ChecklistScreen.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/ui/checklist/ChecklistScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -77,7 +78,7 @@ fun ChecklistScreen(state: ChecklistState, interactor: ChecklistInteractor) {
                     )
                 }
             })
-            StateBox(state = state.screenState, onRefresh = { interactor.refresh() }) {
+            StateBox(modifier = Modifier.fillMaxWidth().height(300.dp), state = state.screenState, onRefresh = { interactor.refresh() }) {
                 if (state.currentCheck != null && state.currentCheckDetails != null) {
                     Column(
                         modifier = Modifier
