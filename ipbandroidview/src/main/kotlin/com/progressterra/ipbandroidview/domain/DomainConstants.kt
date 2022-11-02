9645b155874c4f80b8fa6320834ae2596144ba76
velvet <berhram@protonmail.com>
2022-11-02 09:51:07 +0300

    fix: id

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/domain/DomainConstants.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/domain/DomainConstants.kt
@@ -2,7 +2,7 @@ package com.progressterra.ipbandroidview.domain
 
 object DomainConstants {
 
-    const val MAIN_DEFAULT_CATEGORY_ID = "ef1f4abf-5060-4f19-be1c-4a3f56beb89d"
+    const val MAIN_DEFAULT_CATEGORY_ID = "8292c696-2831-4c34-a2d6-77fefe28e60c"
 
     const val PAGE_SIZE = 10
 }
