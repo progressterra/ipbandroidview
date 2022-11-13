a43c45f36119a86a11b40b256df99a82bdc76235
velvet <berhram@protonmail.com>
2022-11-13 13:07:50 +0300

    feat: new features and optimizations

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/components/topbar/BasicTopAppBar.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/components/topbar/BasicTopAppBar.kt
@@ -2,7 +2,6 @@ package com.progressterra.ipbandroidview.components.topbar
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
