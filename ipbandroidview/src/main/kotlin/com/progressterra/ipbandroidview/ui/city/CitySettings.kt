accc0ea76ddd2ea8877cee7ba29d9611c6cc7c78
velvet <berhram@protonmail.com>
2022-11-23 19:30:55 +0300

    fix: settings parcelable

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/ui/city/CitySettings.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/ui/city/CitySettings.kt
@@ -1,5 +1,9 @@
 package com.progressterra.ipbandroidview.ui.city
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class CitySettings(
     val passable: Boolean
-)
+) : Parcelable
