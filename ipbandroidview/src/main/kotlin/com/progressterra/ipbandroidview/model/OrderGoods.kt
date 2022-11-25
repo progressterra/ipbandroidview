c3743062c317a8bda220dd222e3f2f0ee8ce0691
velvet <berhram@protonmail.com>
2022-11-25 20:45:58 +0300

    fix: parcelation

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/model/OrderGoods.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/model/OrderGoods.kt
@@ -1,7 +1,11 @@
 package com.progressterra.ipbandroidview.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class OrderGoods(
     val id: String,
     val inCartCounter: Int,
     val image: String
-)
+) : Parcelable
