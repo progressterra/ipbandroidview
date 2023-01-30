dad709f07e5b9c0cc29f4cce8ba90d0bd5c10173
velvet <berhram@protonmail.com>
2023-01-30 21:37:18 +0300

    feat: size table UI

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/composable/SizeTableBottomSheet.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/composable/SizeTableBottomSheet.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import com.progressterra.ipbandroidview.R
 import com.progressterra.ipbandroidview.theme.AppTheme
 
@@ -29,12 +31,15 @@ fun SizeTableBottomSheet(
             .background(AppTheme.colors.surfaces)
             .padding(AppTheme.dimensions.medium)
             .fillMaxWidth(),
-        verticalArrangement = Arrangement.spacedBy(AppTheme.dimensions.small)
+        verticalArrangement = Arrangement.spacedBy(AppTheme.dimensions.small),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = stringResource(R.string.size_table),
             color = AppTheme.colors.black,
-            style = AppTheme.typography.text
+            style = AppTheme.typography.title,
+            maxLines = 1,
+            textAlign = TextAlign.Center
         )
     }
     WebViewCompose(url = url)
