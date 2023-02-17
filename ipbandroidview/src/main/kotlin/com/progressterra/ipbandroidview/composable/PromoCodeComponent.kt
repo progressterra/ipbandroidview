f37dd15669a7cef4619dba1d3cc060e4e7b44f3b
velvet <berhram@protonmail.com>
2023-02-17 03:25:43 +0300

    feat: pick point component

---
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/composable/PromoCodeComponent.kt
@@ -0,0 +1,60 @@
+package com.progressterra.ipbandroidview.composable
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.progressterra.ipbandroidview.R
+import com.progressterra.ipbandroidview.composable.component.TextFieldComponent
+import com.progressterra.ipbandroidview.model.SimplePrice
+import com.progressterra.ipbandroidview.theme.AppTheme
+
+@Immutable
+interface PromoCodeState {
+
+    val promoCode: SimplePrice
+
+    val promoCodeName: String
+}
+
+@Composable
+fun PromoCodeComponent(
+    modifier: Modifier = Modifier,
+    state: PromoCodeState,
+    editPromoCode: (String) -> Unit,
+    applyPromoCode: () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .clip(AppTheme.shapes.medium)
+            .background(AppTheme.colors.surfaces)
+            .animateContentSize()
+            .padding(AppTheme.dimensions.medium),
+        verticalArrangement = Arrangement.spacedBy(AppTheme.dimensions.small)
+    ) {
+        TextFieldComponent(
+            modifier = Modifier.fillMaxWidth(),
+            text = state.promoCodeName,
+            hint = stringResource(R.string.promo_code),
+            onChange = editPromoCode,
+            action = applyPromoCode
+        )
+
+        if (!state.promoCode.isEmpty()) {
+            Text(
+                text = "Скидка ${state.promoCode}",
+                color = AppTheme.colors.primary,
+                style = AppTheme.typography.tertiaryText
+            )
+        }
+    }
+}
