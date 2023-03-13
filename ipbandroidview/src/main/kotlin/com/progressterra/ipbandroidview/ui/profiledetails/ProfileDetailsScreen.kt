c227f192633071ef453d2ca78dd42ca87f326c93
velvet <berhram@protonmail.com>
2023-03-14 00:38:01 +0300

    fix: ui

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/ui/profiledetails/ProfileDetailsScreen.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/ui/profiledetails/ProfileDetailsScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.progressterra.ipbandroidview.R
@@ -30,7 +31,7 @@ import com.progressterra.ipbandroidview.composable.component.TextField
 import com.progressterra.ipbandroidview.composable.component.TextFieldState
 import com.progressterra.ipbandroidview.theme.AppTheme
 
-private val logoSize = 32.dp
+private val logoSize = 64.dp
 
 /**
  * name, email, phone - text field
@@ -111,7 +112,8 @@ fun ProfileDetailsScreen(
             Text(
                 text = stringResource(R.string.created_by),
                 color = AppTheme.colors.gray2,
-                style = AppTheme.typography.secondaryText
+                style = AppTheme.typography.secondaryText,
+                textAlign = TextAlign.Center
             )
             Text(
                 text = "${stringResource(R.string.version)}${state.version}",
