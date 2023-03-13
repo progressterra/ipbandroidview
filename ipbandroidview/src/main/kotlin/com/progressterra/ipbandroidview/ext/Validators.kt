c4cab723cf4856841a52a7e0e7dd801cfbbafc8d
velvet <berhram@protonmail.com>
2023-03-13 09:00:56 +0300

    fix: email validation

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/ext/Validators.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/ext/Validators.kt
@@ -1,4 +1,8 @@
 package com.progressterra.ipbandroidview.ext
 
+import androidx.core.util.PatternsCompat
+
+fun String.isEmail(): Boolean = PatternsCompat.EMAIL_ADDRESS.matcher(this).matches()
+
 fun String.isNameAndSurname(): Boolean = matches(Regex("^[А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+\$"))
 
