7409bfcda3dfa5fc97fd4f06a4d6318bbbdd87cd
velvet <berhram@protonmail.com>
2023-04-11 15:34:38 +0300

    feat: di for widget module

---
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/widgets/WidgetModule.kt
@@ -0,0 +1,45 @@
+package com.progressterra.ipbandroidview.widgets
+
+import com.progressterra.ipbandroidview.widgets.bonusestransactions.FetchBonusesTransactionsUseCase
+import com.progressterra.ipbandroidview.widgets.deliverypicker.DeliveryMethodMapper
+import com.progressterra.ipbandroidview.widgets.deliverypicker.FetchAvailableDeliveryUseCase
+import com.progressterra.ipbandroidview.widgets.deliverypicker.SetDeliveryAddressUseCase
+import com.progressterra.ipbandroidview.widgets.proshkagalleries.FetchProshkaGalleriesUseCase
+import com.progressterra.ipbandroidview.widgets.proshkaoffers.FetchOffersUseCase
+import com.progressterra.ipbandroidview.widgets.proshkaoffers.FetchProshkaOffersUseCase
+import org.koin.dsl.module
+
+val widgetsModule = module {
+
+    single<FetchBonusesTransactionsUseCase> {
+        FetchBonusesTransactionsUseCase.Base(
+            get(), get(), get(), get()
+        )
+    }
+
+    single<FetchAvailableDeliveryUseCase> {
+        FetchAvailableDeliveryUseCase.Base(
+            get(), get(), get(), get(), get()
+        )
+    }
+
+    single<DeliveryMethodMapper> {
+        DeliveryMethodMapper.Base(get(), get())
+    }
+
+    single<SetDeliveryAddressUseCase> {
+        SetDeliveryAddressUseCase.Base(get(), get(), get())
+    }
+
+    single<FetchProshkaGalleriesUseCase> {
+        FetchProshkaGalleriesUseCase.Base(get())
+    }
+
+    single<FetchOffersUseCase> {
+        FetchOffersUseCase.Base(get(), get(), get(), get())
+    }
+
+    single<FetchProshkaOffersUseCase> {
+        FetchProshkaOffersUseCase.Base(get())
+    }
+}
