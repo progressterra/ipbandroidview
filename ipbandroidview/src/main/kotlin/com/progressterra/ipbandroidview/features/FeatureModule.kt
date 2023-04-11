7798430cf8b5b9af8b672beb3ff9ed5a50556523
velvet <berhram@protonmail.com>
2023-04-11 15:40:01 +0300

    feat: di for features module

---
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/features/FeatureModule.kt
@@ -0,0 +1,34 @@
+package com.progressterra.ipbandroidview.features
+
+import com.progressterra.ipbandroidview.features.addresssuggestions.ChooseSuggestionUseCase
+import com.progressterra.ipbandroidview.features.addresssuggestions.CurrentLocationSuggestionsUseCase
+import com.progressterra.ipbandroidview.features.addresssuggestions.SuggestionUseCase
+import com.progressterra.ipbandroidview.features.paymentmethod.FetchPaymentMethods
+import com.progressterra.ipbandroidview.features.proshkabonuses.ProshkaBonusesUseCase
+import com.progressterra.ipbandroidview.features.proshkastorecard.ProshkaStoreCardMapper
+import org.koin.dsl.module
+
+val featuresModule = module {
+
+    single<ProshkaStoreCardMapper> {
+        ProshkaStoreCardMapper.Base(get())
+    }
+
+    single<ProshkaBonusesUseCase> {
+        ProshkaBonusesUseCase.Base(get(), get(), get(), get())
+    }
+
+    single<FetchPaymentMethods> {
+        FetchPaymentMethods.Base()
+    }
+
+    single<ChooseSuggestionUseCase> {
+        ChooseSuggestionUseCase.Base(get())
+    }
+
+    single<CurrentLocationSuggestionsUseCase> {
+        CurrentLocationSuggestionsUseCase.Base(get(), get(), get())
+    }
+
+    single<SuggestionUseCase> { SuggestionUseCase.Base(get(), get()) }
+}
