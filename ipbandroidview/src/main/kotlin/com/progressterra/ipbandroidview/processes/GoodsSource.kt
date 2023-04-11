69f0f85a3ba9662b0bd48180dd1d8c5f8ad038a7
velvet <berhram@protonmail.com>
2023-04-11 15:34:24 +0300

    fix: old states

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/processes/GoodsSource.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/processes/GoodsSource.kt
@@ -2,21 +2,20 @@ package com.progressterra.ipbandroidview.processes
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
-import com.progressterra.ipbandroidview.composable.component.StoreCardComponentState
-import com.progressterra.ipbandroidview.processes.store.FetchFavoriteIds
+import com.progressterra.ipbandroidview.features.proshkastorecard.ProshkaStoreCardState
 import com.progressterra.ipbandroidview.processes.goods.FetchGoodsPage
+import com.progressterra.ipbandroidview.processes.store.FetchFavoriteIds
 
 class GoodsSource(
     private val fetchGoodsPage: FetchGoodsPage,
     private val categoryId: String,
     private val fetchFavoriteIds: FetchFavoriteIds
-) : PagingSource<Int, StoreCardComponentState>() {
+) : PagingSource<Int, ProshkaStoreCardState>() {
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, StoreCardComponentState> {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ProshkaStoreCardState> {
         val nextPage = params.key ?: 1
         val response = fetchFavoriteIds().onSuccess { favorites ->
-            fetchGoodsPage(categoryId, nextPage, favorites)
-                .onSuccess {
+            fetchGoodsPage(categoryId, nextPage, favorites).onSuccess {
                     return LoadResult.Page(
                         data = it.second,
                         prevKey = if (nextPage == 1) null else nextPage - 1,
@@ -27,7 +26,7 @@ class GoodsSource(
         return LoadResult.Error(response.exceptionOrNull()!!)
     }
 
-    override fun getRefreshKey(state: PagingState<Int, StoreCardComponentState>): Int? {
+    override fun getRefreshKey(state: PagingState<Int, ProshkaStoreCardState>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
             val anchorPage = state.closestPageToPosition(anchorPosition)
             anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/processes/goods/FetchGoodsPage.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/processes/goods/FetchGoodsPage.kt
@@ -2,39 +2,31 @@ package com.progressterra.ipbandroidview.processes.goods
 
 import com.progressterra.ipbandroidapi.api.iecommerce.core.IECommerceCoreRepository
 import com.progressterra.ipbandroidapi.api.scrm.SCRMRepository
-import com.progressterra.ipbandroidview.entities.GoodsItem
+import com.progressterra.ipbandroidview.features.proshkastorecard.ProshkaStoreCardMapper
+import com.progressterra.ipbandroidview.features.proshkastorecard.ProshkaStoreCardState
 import com.progressterra.ipbandroidview.processes.location.ProvideLocation
 import com.progressterra.ipbandroidview.shared.AbstractUseCase
 
 interface FetchGoodsPage {
 
     suspend operator fun invoke(
-        id: String,
-        pageNumber: Int,
-        favorites: List<String>
-    ): Result<Pair<Int, List<GoodsItem>>>
+        id: String, pageNumber: Int, favorites: List<String>
+    ): Result<Pair<Int, List<ProshkaStoreCardState>>>
 
     class Base(
         scrmRepository: SCRMRepository,
         provideLocation: ProvideLocation,
-        private val mapper: StoreGoodsMapper,
+        private val mapper: ProshkaStoreCardMapper,
         private val eCommerceRepo: IECommerceCoreRepository,
     ) : AbstractUseCase(scrmRepository, provideLocation), FetchGoodsPage {
 
         override suspend fun invoke(
             id: String, pageNumber: Int, favorites: List<String>
-        ): Result<Pair<Int, List<GoodsItem>>> = withToken { token ->
+        ): Result<Pair<Int, List<ProshkaStoreCardState>>> = withToken { token ->
             val result = eCommerceRepo.getProductsByCategory(
-                token,
-                id,
-                pageNumber,
-                10,
-                0,
-                0
+                token, id, pageNumber, 10, 0, 0
             ).getOrThrow()
-            result?.numberCurrentPage!! to result.listProducts!!.map {
-                mapper.map(it, favorites.contains(it.idUnique!!))
-            }
+            result?.numberCurrentPage!! to result.listProducts!!.map { mapper.map(it) }
         }
     }
 }
