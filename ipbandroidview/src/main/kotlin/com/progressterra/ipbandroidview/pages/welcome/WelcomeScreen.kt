032356eb93a68581db1c670c3e334ec342162f36
velvet <berhram@protonmail.com>
2023-04-12 03:09:55 +0300

    fix: ui

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/welcome/WelcomeScreen.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/welcome/WelcomeScreen.kt
@@ -3,6 +3,7 @@ package com.progressterra.ipbandroidview.pages.welcome
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -21,7 +22,9 @@ fun WelcomeScreen(
             verticalArrangement = Arrangement.SpaceBetween
         ) {
             Image(
-                modifier = Modifier.weight(1f),
+                modifier = Modifier
+                    .weight(1f)
+                    .aspectRatio(343f / 547f),
                 painter = painterResource(id = R.drawable.proshka_welcome),
                 contentDescription = null
             )
