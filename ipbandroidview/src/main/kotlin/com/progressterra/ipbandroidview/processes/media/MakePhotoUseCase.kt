0fdba29db43670eab593e60bd6c16ba1739a096d
velvet <berhram@protonmail.com>
2023-04-28 17:19:07 +0300

    debug: log

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/processes/media/MakePhotoUseCase.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/processes/media/MakePhotoUseCase.kt
@@ -1,15 +1,13 @@
 package com.progressterra.ipbandroidview.processes.media
 
-import android.Manifest
 import android.content.Intent
 import android.provider.MediaStore
+import android.util.Log
 import com.progressterra.ipbandroidview.entities.MultisizedImage
-import com.progressterra.ipbandroidview.processes.permission.CheckPermissionUseCase
 import com.progressterra.ipbandroidview.shared.CreateId
 import com.progressterra.ipbandroidview.shared.FileExplorer
 import com.progressterra.ipbandroidview.shared.activity.MakePhotoContract
 import com.progressterra.ipbandroidview.shared.activity.PhotoWasNotTakenException
-import com.progressterra.ipbandroidview.shared.throwOnFailure
 
 interface MakePhotoUseCase {
 
@@ -18,26 +16,25 @@ interface MakePhotoUseCase {
     class Base(
         private val makePhotoContract: MakePhotoContract.Client,
         private val fileExplorer: FileExplorer,
-        private val createId: CreateId,
-        private val checkPermissionUseCase: CheckPermissionUseCase
+        private val createId: CreateId
     ) : MakePhotoUseCase {
 
         override suspend fun invoke(): Result<MultisizedImage> = runCatching {
-            checkPermissionUseCase(Manifest.permission.CAMERA).throwOnFailure()
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            Log.d("CAMERA", "$intent")
             val newPhotoId = createId()
+            Log.d("CAMERA", "$newPhotoId")
             val uri = fileExplorer.uriForFile(fileExplorer.pictureFile(newPhotoId))
+            Log.d("CAMERA", "$uri")
             intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
-            if (makePhotoContract.makePhoto(intent))
-                MultisizedImage(
-                    id = newPhotoId,
-                    local = true,
-                    toRemove = false,
-                    thumbnail = uri.toString(),
-                    fullSize = uri.toString()
-                )
-            else
-                throw PhotoWasNotTakenException()
+            if (makePhotoContract.makePhoto(intent)) MultisizedImage(
+                id = newPhotoId,
+                local = true,
+                toRemove = false,
+                thumbnail = uri.toString(),
+                fullSize = uri.toString()
+            )
+            else throw PhotoWasNotTakenException()
         }
     }
 }
