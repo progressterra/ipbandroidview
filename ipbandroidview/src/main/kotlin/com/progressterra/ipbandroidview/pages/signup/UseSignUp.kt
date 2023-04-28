954aff146d0c20d157a00943cb6ea603fdc442a5
velvet <berhram@protonmail.com>
2023-04-28 16:23:21 +0300

    fix: di

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/signup/UseSignUp.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/signup/UseSignUp.kt
@@ -1,6 +1,7 @@
 package com.progressterra.ipbandroidview.pages.signup
 
 import com.progressterra.ipbandroidview.features.authorskip.UseAuthOrSkip
+import com.progressterra.ipbandroidview.features.makephoto.MakePhotoEvent
 import com.progressterra.ipbandroidview.features.topbar.TopBarEvent
 import com.progressterra.ipbandroidview.features.topbar.UseTopBar
 import com.progressterra.ipbandroidview.shared.ui.button.ButtonEvent
@@ -10,6 +11,8 @@ import com.progressterra.ipbandroidview.widgets.edituser.UseEditUser
 interface UseSignUp : UseTopBar, UseEditUser, UseAuthOrSkip {
 
     class Empty : UseSignUp {
+
+        override fun handle(event: MakePhotoEvent) = Unit
 
         override fun handle(event: TopBarEvent) = Unit
 
