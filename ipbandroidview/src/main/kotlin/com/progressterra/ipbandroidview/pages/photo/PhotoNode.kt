55a437fc35ea0bc0db07eec3e3bbd83d4f4321bb
velvet <berhram@protonmail.com>
2023-04-28 16:36:55 +0300

    fix: behavior photo node

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/photo/PhotoNode.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/photo/PhotoNode.kt
@@ -9,36 +9,13 @@ import androidx.compose.ui.Modifier
 import com.bumble.appyx.core.modality.BuildContext
 import com.bumble.appyx.core.node.Node
 import com.progressterra.ipbandroidview.entities.MultisizedImage
-import com.progressterra.ipbandroidview.shared.nav.Back
 import org.koin.androidx.compose.getViewModel
 import org.orbitmvi.orbit.compose.collectAsState
 import org.orbitmvi.orbit.compose.collectSideEffect
 
-interface PhotoNodeContract : Back {
-
-    fun enabled(): Boolean
-
-    fun picture(): MultisizedImage
-
-    data class Base(
-        private val enabled: Boolean,
-        private val picture: MultisizedImage,
-        private val onBack: () -> Unit,
-        private val onRemove: () -> Unit
-    ) : PhotoNodeContract {
-
-        override fun enabled(): Boolean = enabled
-
-        override fun picture(): MultisizedImage = picture
-
-        override fun back() = onBack()
-    }
-}
-
 @Suppress("unused")
 class PhotoNode(
-    buildContext: BuildContext,
-    private val contract: PhotoNodeContract
+    buildContext: BuildContext, private val image: MultisizedImage, private val onBack: () -> Unit
 ) : Node(buildContext) {
 
     @Composable
@@ -46,20 +23,19 @@ class PhotoNode(
         val viewModel: PhotoViewModel = getViewModel()
         viewModel.collectSideEffect {
             when (it) {
-                is PhotoEvent.Back -> contract.back()
+                is PhotoEvent.Back -> onBack()
             }
         }
-        var alreadyLaunched by rememberSaveable(contract) {
+        var alreadyLaunched by rememberSaveable(image) {
             mutableStateOf(false)
         }
         if (!alreadyLaunched) {
             alreadyLaunched = true
-            viewModel.setPhoto(contract.picture())
+            viewModel.setPhoto(image)
         }
         val state = viewModel.collectAsState()
         PhotoScreen(
-            state = state.value,
-            useComponent = viewModel
+            state = state.value, useComponent = viewModel
         )
     }
 }
