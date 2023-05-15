2a4fa1044e64ac27e985247c24351ab0d02a3c4d
velvet <berhram@protonmail.com>
2023-05-15 09:12:04 +0300

    Revert "Revert "chore: cleaning""
    This reverts commit ad53e572966b81eb9df2213b9a79a014e64c2c8c.

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/di/ViewModels.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/di/ViewModels.kt
@@ -1,41 +1,22 @@
 package com.progressterra.ipbandroidview.di
 
 import com.progressterra.ipbandroidview.ui.agreement.AgreementViewModel
-import com.progressterra.ipbandroidview.ui.bonuses.BonusesViewModel
-import com.progressterra.ipbandroidview.ui.bonusesclarification.BonusesClarificationViewModel
-import com.progressterra.ipbandroidview.ui.cart.CartViewModel
-import com.progressterra.ipbandroidview.ui.catalog.CatalogViewModel
 import com.progressterra.ipbandroidview.ui.checklist.ChecklistViewModel
-import com.progressterra.ipbandroidview.ui.city.CityViewModel
 import com.progressterra.ipbandroidview.ui.confirmationcode.ConfirmationCodeViewModel
 import com.progressterra.ipbandroidview.ui.documents.DocumentsViewModel
-import com.progressterra.ipbandroidview.ui.favorites.FavoritesViewModel
-import com.progressterra.ipbandroidview.ui.goods.GoodsViewModel
-import com.progressterra.ipbandroidview.ui.goodsdetails.GoodsDetailsViewModel
-import com.progressterra.ipbandroidview.ui.main.MainViewModel
 import com.progressterra.ipbandroidview.ui.mainhaccp.MainHaccpViewModel
-import com.progressterra.ipbandroidview.ui.order.OrderViewModel
-import com.progressterra.ipbandroidview.ui.orderprocessing.OrderProcessingViewModel
-import com.progressterra.ipbandroidview.ui.orders.OrdersViewModel
 import com.progressterra.ipbandroidview.ui.organizationaudits.OrganizationAuditsViewModel
 import com.progressterra.ipbandroidview.ui.organizations.OrganizationsViewModel
 import com.progressterra.ipbandroidview.ui.partner.PartnerViewModel
 import com.progressterra.ipbandroidview.ui.photo.PhotoViewModel
-import com.progressterra.ipbandroidview.ui.pickuppoint.PickUpPointViewModel
 import com.progressterra.ipbandroidview.ui.profile.ProfileViewModel
 import com.progressterra.ipbandroidview.ui.profiledetails.ProfileDetailsViewModel
-import com.progressterra.ipbandroidview.ui.referral.ReferralViewModel
 import com.progressterra.ipbandroidview.ui.signin.SignInViewModel
-import com.progressterra.ipbandroidview.ui.signup.SignUpViewModel
 import com.progressterra.ipbandroidview.ui.splash.SplashViewModel
-import com.progressterra.ipbandroidview.ui.subcatalog.SubCatalogViewModel
-import com.progressterra.ipbandroidview.ui.support.SupportViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelsModule = module {
-
-    viewModel { MainViewModel(get(), get(), get(), get(), get()) }
 
     viewModel { DocumentsViewModel(get()) }
 
@@ -45,13 +26,10 @@ val viewModelsModule = module {
 
     viewModel { SignInViewModel(get(), get(), get()) }
 
-    viewModel { SignUpViewModel(get(), get(), get(), get(), get(), get()) }
 
     viewModel { ConfirmationCodeViewModel(get(), get(), get()) }
 
     viewModel { OrganizationAuditsViewModel(get(), get()) }
-
-    viewModel { CityViewModel(get(), get(), get(), get(), get()) }
 
     viewModel { AgreementViewModel(get()) }
 
@@ -82,47 +60,6 @@ val viewModelsModule = module {
     viewModel { ProfileViewModel(get(), get()) }
 
     viewModel { ProfileDetailsViewModel(get(), get(), get(), get(), get(), get(), get(), get()) }
-
-    viewModel { CatalogViewModel(get()) }
-
-    viewModel { SubCatalogViewModel() }
-
-    viewModel { GoodsViewModel(get(), get(), get()) }
-
-    viewModel { GoodsDetailsViewModel(get(), get(), get(), get()) }
-
-    viewModel { FavoritesViewModel(get(), get()) }
-
-    viewModel { CartViewModel(get(), get(), get(), get()) }
-
-    viewModel { BonusesViewModel(get(), get()) }
-
-    viewModel { BonusesClarificationViewModel() }
-
-    viewModel {
-        OrderViewModel(
-            get(),
-            get(),
-            get(),
-            get(),
-            get(),
-            get(),
-            get(),
-            get(),
-            get(),
-            get()
-        )
-    }
-
-    viewModel { OrdersViewModel(get()) }
-
-    viewModel { OrderProcessingViewModel() }
-
-    viewModel { PickUpPointViewModel(get()) }
-
-    viewModel { SupportViewModel(get(), get(), get()) }
-
-    viewModel { ReferralViewModel(get(), get(), get()) }
 
     viewModel { PartnerViewModel(get(), get()) }
 
