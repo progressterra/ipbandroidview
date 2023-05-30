16dd794eca211ef5114f5c8b5f0e54c9ea955aaf
velvet <berhram@protonmail.com>
2023-05-30 20:05:41 +0300

    fix: debug

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/domain/usecase/checklist/AllOrganizationsUseCase.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/domain/usecase/checklist/AllOrganizationsUseCase.kt
@@ -1,5 +1,6 @@
 package com.progressterra.ipbandroidview.domain.usecase.checklist
 
+import android.util.Log
 import com.progressterra.ipbandroidapi.api.checklist.ChecklistRepository
 import com.progressterra.ipbandroidapi.api.complace.ComPlaceRepository
 import com.progressterra.ipbandroidapi.api.scrm.SCRMRepository
@@ -26,21 +27,22 @@ interface AllOrganizationsUseCase {
         private val noData = manageResources.string(R.string.no_data)
 
         override suspend fun invoke(): Result<List<Organization>> = withToken { token ->
+            Log.d("Places", "start use case")
             val places = placeRepository.places(
                 token, ComPlaceF(emptyList(), null, "", 0, 300)
-            ).getOrThrow()
-
+            ).onFailure { Log.e("Places", it.message, it) }.getOrThrow()!!
             buildList {
-                places?.map { place ->
+                places.map { place ->
                     val audits =
                         checklistRepository.availableChecklistsForPlace(token, place.idUnique!!)
+                            .onFailure { Log.e("Places", it.message, it) }
                             .getOrThrow()!!
                     val docs =
                         checklistRepository.availableDocsForPlace(
                             token, place.idUnique!!, ChecklistF(
                                 emptyList(), null, "", false, 0, 300
                             )
-                        ).getOrThrow()!!
+                        ).onFailure { Log.e("Places", it.message, it) }.getOrThrow()!!
                     add(
                         Organization(
                             address = place.address ?: noData,
