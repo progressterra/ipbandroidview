2b610f455a1afc4cdebcc6464462ae3ab696fab9
velvet <berhram@protonmail.com>
2023-06-09 10:27:51 +0300

    fix: serialization

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/entities/Citizenship.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/entities/Citizenship.kt
@@ -1,9 +1,12 @@
 package com.progressterra.ipbandroidview.entities
 
+import com.google.gson.annotations.SerializedName
 import com.progressterra.ipbandroidview.shared.IsEmpty
 
 data class Citizenship(
+    @SerializedName("name")
     val name: String = "",
+    @SerializedName("id")
     override val id: String = ""
 ) : Id, IsEmpty {
 
