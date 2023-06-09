5d3cf5d4ce1dedd671adfba3c5e416e2e3ff6b57
velvet <berhram@protonmail.com>
2023-06-09 06:07:19 +0300

    fix: documents use case

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/widgets/documents/DocumentsUseCase.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/widgets/documents/DocumentsUseCase.kt
@@ -1,11 +1,16 @@
 package com.progressterra.ipbandroidview.widgets.documents
 
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
+import com.google.gson.reflect.TypeToken
 import com.progressterra.ipbandroidapi.api.documents.DocumentsRepository
 import com.progressterra.ipbandroidapi.api.documents.models.TypeStatusDoc
+import com.progressterra.ipbandroidapi.api.documents.models.TypeValueCharacteristic
 import com.progressterra.ipbandroidapi.api.scrm.SCRMRepository
 import com.progressterra.ipbandroidview.processes.location.ProvideLocation
 import com.progressterra.ipbandroidview.shared.AbstractTokenUseCase
 import com.progressterra.ipbandroidview.shared.UserData
+import com.progressterra.ipbandroidview.shared.ui.textfield.TextFieldState
 
 interface DocumentsUseCase {
 
@@ -14,20 +19,45 @@ interface DocumentsUseCase {
     class Base(
         provideLocation: ProvideLocation,
         scrmRepository: SCRMRepository,
-        val repo: DocumentsRepository
+        private val repo: DocumentsRepository,
+        private val gson: Gson,
     ) : AbstractTokenUseCase(scrmRepository, provideLocation), DocumentsUseCase {
 
         override suspend fun invoke(): Result<DocumentsState> = withToken { token ->
-            DocumentsState(
-                items = repo.docsBySpecification(token, UserData.docSpecId)
-                    .getOrThrow()?.listProductCharacteristic?.map {
-                        DocumentsState.Item(
-                            status = it.characteristicValue?.statusDoc ?: TypeStatusDoc.NOT_FILL,
-                            name = it.characteristicType?.name ?: "",
-                            id = it.characteristicValue?.idUnique!!
+            DocumentsState(items = repo.docsBySpecification(token, UserData.docSpecId)
+                .getOrThrow()?.listProductCharacteristic?.map { doc ->
+                    val entries = gson.fromJson<List<FieldData>?>(
+                        doc.characteristicType?.dataInJSON,
+                        object : TypeToken<List<FieldData>>() {}.type
+                    ).map {
+                        TextFieldState(
+                            id = "doc${it.idrfCharacteristicType}",
+                            text = it.valueData,
+                            hint = it.comment
                         )
-                    } ?: emptyList()
-            )
+                    }
+                    DocumentsState.Item(
+                        status = doc.characteristicValue?.statusDoc ?: TypeStatusDoc.NOT_FILL,
+                        name = doc.characteristicType?.name ?: "",
+                        id = doc.characteristicValue?.idUnique!!,
+                        entries = entries
+                    )
+                } ?: emptyList())
         }
+
+        data class FieldData(
+            // Значение харатеристики (к какому типу документа относится поле), на основе который создано поле
+            @SerializedName("idrfCharacteristicType") val idrfCharacteristicType: String,
+            // Наименование поля
+            @SerializedName("name") val name: String,
+            // Подсказка, которую можно отобразить пользователю
+            @SerializedName("comment") val comment: String,
+            // Позволяет организовать сортировку данных
+            @SerializedName("order") val order: Int,
+            // Тип значения поля. Позволяет понять какие данные хранятся в поле. Позволяет правильного отображать значения полей
+            @SerializedName("typeValue") val typeValue: TypeValueCharacteristic,
+            // Само значение
+            @SerializedName("valueData") val valueData: String
+        )
     }
 }
