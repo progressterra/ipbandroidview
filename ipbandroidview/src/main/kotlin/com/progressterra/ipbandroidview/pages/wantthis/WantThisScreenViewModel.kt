bfff309d3ebd2d285f1852c6b3f506e81cafbe64
velvet <berhram@protonmail.com>
2023-06-21 07:31:57 +0300

    fix: ux

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/wantthis/WantThisScreenViewModel.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/wantthis/WantThisScreenViewModel.kt
@@ -68,7 +68,7 @@ class WantThisScreenViewModel(
                     postSideEffect(WantThisScreenEvent.Back)
                 }
 
-                "cancel" -> reduce { state }
+                "cancel" -> postSideEffect(WantThisScreenEvent.Back)
             }
         }
     }
