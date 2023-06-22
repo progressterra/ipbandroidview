3a58446cef043ba7de362fa66813bd25246c84d0
velvet <berhram@protonmail.com>
2023-06-22 10:25:17 +0300

    fix: renaming

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/features/buygoods/BuyGoodsEvent.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/features/buygoods/BuyGoodsEvent.kt
@@ -2,7 +2,7 @@ package com.progressterra.ipbandroidview.features.buygoods
 
 sealed class BuyGoodsEvent(val id: String) {
 
-    class BuyWithLoan(id: String) : BuyGoodsEvent(id)
+    class BuyInstallment(id: String) : BuyGoodsEvent(id)
 
     class Buy(id: String) : BuyGoodsEvent(id)
 }
