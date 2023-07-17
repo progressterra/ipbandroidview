9fef7a69608c9d5e4b4f3a28abc3fc37fe088126
velvet <berhram@proton.me>
2023-07-17 22:50:38 +0300

    fix: validation

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/shared/StringExt.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/shared/StringExt.kt
@@ -9,7 +9,7 @@ fun String.isEmail() = PatternsCompat.EMAIL_ADDRESS.matcher(this).matches()
 
 fun String.isNameAndSurname() = matches(Regex("^[А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+\$"))
 
-fun String.isRussianPhoneNumber() = matches(Regex("^7\\d{10}$"))
+fun String.isRussianPhoneNumber() = matches(Regex("^\\d{10}$"))
 
 fun String.isTestPhoneNumber() = this == "1777555777"
 
