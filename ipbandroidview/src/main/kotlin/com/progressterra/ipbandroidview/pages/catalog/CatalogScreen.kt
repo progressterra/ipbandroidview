ccfc98b92e46796826504b619ae77d32493f2599
velvet <berhram@proton.me>
2023-07-30 02:12:21 +0300

    fix: correct paging

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/catalog/CatalogScreen.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/catalog/CatalogScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.paging.PagingData
 import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.itemKey
 import com.progressterra.ipbandroidview.entities.SimplePrice
 import com.progressterra.ipbandroidview.features.catalogcard.CatalogCard
 import com.progressterra.ipbandroidview.features.catalogcard.CatalogCardState
@@ -66,7 +67,8 @@ fun CatalogScreen(
                         contentPadding = PaddingValues(start = 20.dp, end = 20.dp, top = 40.dp)
                     ) {
                         items(
-                            count = lazyItems.itemCount
+                            count = lazyItems.itemCount,
+                            key = lazyItems.itemKey { it.id }
                         ) { index ->
                             lazyItems[index]?.let {
                                 Box(contentAlignment = Alignment.Center) {
