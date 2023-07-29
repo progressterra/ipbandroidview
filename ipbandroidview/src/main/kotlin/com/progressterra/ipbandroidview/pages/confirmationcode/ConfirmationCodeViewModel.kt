5d2339b02f593de90062223780469126283f065d
velvet <berhram@proton.me>
2023-07-29 04:25:17 +0300

    feat: confirmation code new arch

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/confirmationcode/ConfirmationCodeViewModel.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/confirmationcode/ConfirmationCodeViewModel.kt
@@ -1,86 +1,66 @@
 package com.progressterra.ipbandroidview.pages.confirmationcode
 
-import androidx.lifecycle.ViewModel
 import com.progressterra.ipbandroidview.R
 import com.progressterra.ipbandroidview.features.code.CodeEvent
-import com.progressterra.ipbandroidview.features.code.code
-import com.progressterra.ipbandroidview.features.code.enabled
-import com.progressterra.ipbandroidview.features.code.phone
 import com.progressterra.ipbandroidview.features.countdown.CountdownEvent
-import com.progressterra.ipbandroidview.features.countdown.count
-import com.progressterra.ipbandroidview.features.countdown.enabled
 import com.progressterra.ipbandroidview.features.topbar.TopBarEvent
 import com.progressterra.ipbandroidview.processes.auth.StartVerificationChannelUseCase
+import com.progressterra.ipbandroidview.shared.BaseViewModel
 import kotlinx.coroutines.delay
-import org.orbitmvi.orbit.Container
-import org.orbitmvi.orbit.ContainerHost
-import org.orbitmvi.orbit.annotation.OrbitExperimental
-import org.orbitmvi.orbit.syntax.simple.blockingIntent
-import org.orbitmvi.orbit.syntax.simple.intent
-import org.orbitmvi.orbit.syntax.simple.postSideEffect
-import org.orbitmvi.orbit.syntax.simple.reduce
-import org.orbitmvi.orbit.viewmodel.container
 
-@OptIn(OrbitExperimental::class)
 class ConfirmationCodeViewModel(
     private val startVerificationChannelUseCase: StartVerificationChannelUseCase,
     private val endVerificationChannelUseCase: EndVerificationChannelUseCase
-) : ViewModel(), ContainerHost<ConfirmationCodeState, ConfirmationCodeEvent>, UseConfirmationCode {
+) : BaseViewModel<ConfirmationCodeState, ConfirmationCodeEvent>(), UseConfirmationCode {
 
-    override val container: Container<ConfirmationCodeState, ConfirmationCodeEvent> =
-        container(ConfirmationCodeState())
+    override val initialState = ConfirmationCodeState()
 
     fun refresh(phoneNumber: String) {
-        intent {
-            reduce { ConfirmationCodeState.code.code.set(state, "") }
-            reduce { ConfirmationCodeState.code.phone.set(state, phoneNumber) }
-            startTimer()
-        }
+        fastEmitState { it.copy(code = it.code.copy(code = "", phone = phoneNumber)) }
+        startTimer()
     }
 
     private fun onNext() {
-        intent {
-            reduce { ConfirmationCodeState.code.enabled.set(state, false) }
-            val call = endVerificationChannelUseCase(state.code.phone, state.code.code).onSuccess {
-                postSideEffect(ConfirmationCodeEvent.Next)
+        onBackground {
+            emitState { it.copy(code = it.code.copy(enabled = false)) }
+            val call = endVerificationChannelUseCase(
+                state.value.code.phone,
+                state.value.code.code
+            ).onSuccess {
+                postEffect(ConfirmationCodeEvent.Next)
             }.onFailure {
-                postSideEffect(ConfirmationCodeEvent.Toast(R.string.wrong_code))
+                postEffect(ConfirmationCodeEvent.Toast(R.string.wrong_code))
             }
-            reduce { ConfirmationCodeState.code.enabled.set(state, call.isSuccess) }
+            emitState { it.copy(code = it.code.copy(enabled = call.isSuccess)) }
         }
     }
 
     private fun startTimer() {
-        intent {
-            reduce { ConfirmationCodeState.repeat.enabled.set(state, false) }
+        onBackground {
+            emitState { it.copy(repeat = it.repeat.copy(enabled = false)) }
             for (i in 45.downTo(1)) {
                 delay(1000)
-                reduce {
-                    ConfirmationCodeState.repeat.count.set(
-                        state,
-                        if (i >= 10) "00:$i" else "00:0$i"
-                    )
-                }
+                emitState { it.copy(repeat = it.repeat.copy(count = if (i >= 10) "00:$i" else "00:0$i")) }
             }
-            reduce { ConfirmationCodeState.repeat.enabled.set(state, true) }
+            emitState { it.copy(repeat = it.repeat.copy(enabled = true)) }
         }
     }
 
     override fun handle(event: CodeEvent) {
-        blockingIntent {
-            reduce { ConfirmationCodeState.code.code.set(state, event.code) }
-            if (event.code.length == 4) onNext()
+        fastEmitState {
+            it.copy(code = it.code.copy(code = event.code))
         }
+        if (event.code.length == 4) onNext()
     }
 
     override fun handle(event: CountdownEvent) {
-        intent {
-            startVerificationChannelUseCase(state.code.phone)
+        onBackground {
+            startVerificationChannelUseCase(state.value.code.phone)
             startTimer()
         }
     }
 
     override fun handle(event: TopBarEvent) {
-        intent { postSideEffect(ConfirmationCodeEvent.Back) }
+        postEffect(ConfirmationCodeEvent.Back)
     }
 }
--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/widgets/storeitems/StoreItemsState.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/widgets/storeitems/StoreItemsState.kt
@@ -1,14 +1,10 @@
 package com.progressterra.ipbandroidview.widgets.storeitems
 
 import androidx.paging.PagingData
-import arrow.optics.optics
 import com.progressterra.ipbandroidview.features.storecard.StoreCardState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
 
-@optics
 data class StoreItemsState(
     val items: Flow<PagingData<StoreCardState>> = emptyFlow()
-) {
-    companion object
-}
+)
