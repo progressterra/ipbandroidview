3cda51499dfa57d710572cc448ad61aaf1428138
velvet <berhram@proton.me>
2023-08-01 16:09:39 +0300

    build: deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,13 @@ buildscript {
     }
     dependencies {
         classpath("com.google.gms:google-services:4.3.15")
-        classpath("com.android.tools.build:gradle:8.0.2")
+        classpath("com.android.tools.build:gradle:8.1.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
 }
 
 plugins {
-    id("com.android.library") version "8.0.2" apply false
+    id("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("org.jetbrains.kotlin.jvm") version "1.9.0" apply false
 }
