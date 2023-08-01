5e0596ea9e1666719e2281c8a78aa56e423102dd
velvet <berhram@proton.me>
2023-08-01 10:08:01 +0300

    build: deps, ver bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,13 +12,13 @@ buildscript {
 }
 
 plugins {
-    id("com.android.library") version "8.0.0" apply false
+    id("com.android.library") version "8.0.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("org.jetbrains.kotlin.jvm") version "1.9.0" apply false
 }
 
 allprojects {
-    version = "0.18.44"
+    version = "0.18.45"
     group = "com.progressterra.ipbandroidview"
     repositories {
         google()
