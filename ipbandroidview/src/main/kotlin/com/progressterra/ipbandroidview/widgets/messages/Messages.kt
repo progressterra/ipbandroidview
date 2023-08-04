5349a738c252008c9940e5d37c1f9b7dee3f5734
velvet <berhram@proton.me>
2023-08-04 12:58:39 +0300

    fix: messages layout

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/widgets/messages/Messages.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/widgets/messages/Messages.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -65,8 +66,9 @@ fun Messages(
 
     val lazyItems = state.items.collectAsLazyPagingItems()
     LazyColumn(
-        modifier = modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.spacedBy(space = 8.dp, alignment = Alignment.Bottom)
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(space = 8.dp, alignment = Alignment.Bottom),
+        reverseLayout = true
     ) {
         items(
             count = lazyItems.itemCount,
