b69ce1a2575dd7266257192b1ea04a7ab1fc3e78
velvet <berhram@proton.me>
2023-08-15 14:54:13 +0300

    feat: screens' and components' interactor contracts

---
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/features/bankcard/UseBankCard.kt
@@ -0,0 +1,6 @@
+package com.progressterra.ipbandroidview.features.bankcard
+
+interface UseBankCard {
+
+    fun handleEvent(event: BankCardEvent)
+}
---
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/bankcards/UseBankCardsScreen.kt
@@ -0,0 +1,8 @@
+package com.progressterra.ipbandroidview.pages.bankcards
+
+import com.progressterra.ipbandroidview.features.bankcard.UseBankCard
+import com.progressterra.ipbandroidview.features.topbar.UseTopBar
+import com.progressterra.ipbandroidview.shared.ui.button.UseButton
+import com.progressterra.ipbandroidview.shared.ui.statebox.UseStateBox
+
+interface UseBankCardsScreen : UseTopBar, UseStateBox, UseButton, UseBankCard
