1da53eeb4fb32b24799365c077f002522e687190
velvet <berhram@proton.me>
2023-08-21 19:48:23 +0300

    fix: order details ux

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/features/orderdetails/OrderDetailsState.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/features/orderdetails/OrderDetailsState.kt
@@ -1,24 +1,24 @@
 package com.progressterra.ipbandroidview.features.orderdetails
 
+import androidx.compose.runtime.Immutable
 import com.progressterra.ipbandroidapi.api.cart.models.TypeStatusOrder
 import com.progressterra.ipbandroidview.entities.SimplePrice
 import com.progressterra.ipbandroidview.features.ordertracking.OrderTrackingState
 import com.progressterra.ipbandroidview.widgets.orderitems.OrderItemsState
 
+@Immutable
 data class OrderDetailsState(
-    val id: String = "",
-    val number: String = "",
-    val status: TypeStatusOrder = TypeStatusOrder.CANCELED,
-    val date: String = "",
-    val count: Int = 0,
-    val totalPrice: SimplePrice = SimplePrice(),
-    val goods: OrderItemsState = OrderItemsState()
+    val id: String,
+    val number: String,
+    val status: TypeStatusOrder,
+    val date: String,
+    val count: Int,
+    val totalPrice: SimplePrice,
+    val goods: OrderItemsState
 ) {
 
     fun toOrderTrackingState() = OrderTrackingState(
         status = status,
         number = number
     )
-
-    companion object
 }
