28ee23d121272a5dceaab2bdaee7e21550405a65
velvet <berhram@proton.me>
2023-08-23 13:26:22 +0300

    feat: state column improved

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/shared/ui/statebox/StateBoxState.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/shared/ui/statebox/StateBoxState.kt
@@ -1,10 +1,14 @@
 package com.progressterra.ipbandroidview.shared.ui.statebox
 
+import android.os.Parcelable
 import androidx.compose.runtime.Immutable
 import com.progressterra.ipbandroidview.entities.Id
+import kotlinx.parcelize.Parcelize
 
+//TODO rename to StateColumnState
 @Immutable
+@Parcelize
 data class StateBoxState(
     override val id: String = "",
     val state: ScreenState = ScreenState.LOADING
-) : Id
+) : Id, Parcelable
