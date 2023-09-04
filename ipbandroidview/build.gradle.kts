7cca5cd5be2d2f86830ae2a44276ec9e31a6c26f
velvet <berhram@proton.me>
2023-09-05 02:36:39 +0300

    build: deps

--- ipbandroidview/build.gradle.kts
+++ ipbandroidview/build.gradle.kts
@@ -117,7 +117,7 @@ dependencies {
     api("com.google.android.gms:play-services-maps:18.1.0")
 
     // iProBonusAndroidAPI
-    api("com.progressterra.ipbandroidapi:ipbandroidapi:0.5.43")
+    api("com.progressterra.ipbandroidapi:ipbandroidapi:0.5.45")
 
     // Kotpref
     api("com.chibatching.kotpref:kotpref:2.13.2")
