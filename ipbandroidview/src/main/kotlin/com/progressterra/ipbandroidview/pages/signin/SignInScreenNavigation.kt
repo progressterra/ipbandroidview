b1facb331d29ecfaab6246dae1c8570b95e0e96e
velvet <berhram@proton.me>
2023-09-05 02:38:18 +0300

    fix: nav

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/signin/SignInScreenNavigation.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/signin/SignInScreenNavigation.kt
@@ -1,8 +1,10 @@
 package com.progressterra.ipbandroidview.pages.signin
 
+import com.progressterra.ipbandroidview.entities.SignInData
+
 interface SignInScreenNavigation {
 
-    fun onNext(data: String)
+    fun onNext(data: SignInData)
 
     fun onSkip()
 }
