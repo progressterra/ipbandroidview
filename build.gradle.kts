1e29f4a675fcaf63b3b57246cb147b1c47c509ce
velvet <berhram@proton.me>
2023-09-05 11:38:51 +0300

    build: ver bump & deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 }
 
 allprojects {
-    version = "0.19.7"
+    version = "0.19.8"
     group = "com.progressterra.ipbandroidview"
     repositories {
         google()
--- ipbandroidview/build.gradle.kts
+++ ipbandroidview/build.gradle.kts
@@ -117,7 +117,7 @@ dependencies {
     api("com.google.android.gms:play-services-maps:18.1.0")
 
     // iProBonusAndroidAPI
-    api("com.progressterra.ipbandroidapi:ipbandroidapi:0.5.45")
+    api("com.progressterra.ipbandroidapi:ipbandroidapi:0.5.46")
 
     // Kotpref
     api("com.chibatching.kotpref:kotpref:2.13.2")
