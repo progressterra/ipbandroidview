e7c5c044834523ea910921d349bb803a0e8eece8
velvet <berhram@proton.me>
2023-09-05 10:17:51 +0300

    fix: shared side effect flow

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/shared/mvi/AbstractViewModel.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/shared/mvi/AbstractViewModel.kt
@@ -12,14 +12,10 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.BroadcastChannel
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
 abstract class AbstractViewModel<S : Any, E : Any> : ViewModel(), Operations {
