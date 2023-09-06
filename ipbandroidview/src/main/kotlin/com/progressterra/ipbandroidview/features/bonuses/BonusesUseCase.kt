ba49e88ca6f18f3fc1e0076c391d6d97fc0a66d3
velvet <berhram@proton.me>
2023-09-06 09:33:04 +0300

    fix: bonuses temporally disabled

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/features/bonuses/BonusesUseCase.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/features/bonuses/BonusesUseCase.kt
@@ -1,11 +1,8 @@
 package com.progressterra.ipbandroidview.features.bonuses
 
 import com.progressterra.ipbandroidapi.api.balance.BalanceRepository
-import com.progressterra.ipbandroidapi.api.ibonus.IBonusRepository
-import com.progressterra.ipbandroidview.R
 import com.progressterra.ipbandroidview.processes.ObtainAccessToken
 import com.progressterra.ipbandroidview.shared.AbstractTokenUseCase
-import com.progressterra.ipbandroidview.shared.ManageResources
 
 interface BonusesUseCase {
 
@@ -13,17 +10,12 @@ interface BonusesUseCase {
 
     class Base(
         obtainAccessToken: ObtainAccessToken,
-        manageResources: ManageResources,
-        private val bonusesRepository: IBonusRepository,
         private val balanceRepository: BalanceRepository
     ) : BonusesUseCase, AbstractTokenUseCase(obtainAccessToken) {
 
-        private val noData = manageResources.string(R.string.no_data)
-
         override suspend fun invoke(): Result<BonusesState> = withToken { token ->
-            val response = bonusesRepository.getGeneralInfo(token).getOrThrow()
             BonusesState(
-                bonuses = response?.currentQuantity?.toInt()?.toString() ?: noData,
+                bonuses = "0",
                 roubles = balanceRepository.client(token).getOrThrow()?.amount?.toInt()?.toString()
                     ?: ""
             )
--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/widgets/bonusestransactions/FetchBonusesTransactionsUseCase.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/widgets/bonusestransactions/FetchBonusesTransactionsUseCase.kt
@@ -1,14 +1,7 @@
 package com.progressterra.ipbandroidview.widgets.bonusestransactions
 
-import com.progressterra.ipbandroidapi.api.ibonus.IBonusRepository
-import com.progressterra.ipbandroidview.R
-import com.progressterra.ipbandroidview.entities.formatZdt
-import com.progressterra.ipbandroidview.entities.parseToZDT
-import com.progressterra.ipbandroidview.features.bonustransaction.BonusTransactionState
-import com.progressterra.ipbandroidview.features.bonustransaction.BonusTransactionType
 import com.progressterra.ipbandroidview.processes.ObtainAccessToken
 import com.progressterra.ipbandroidview.shared.AbstractTokenUseCase
-import com.progressterra.ipbandroidview.shared.ManageResources
 
 interface FetchBonusesTransactionsUseCase {
 
@@ -16,25 +9,10 @@ interface FetchBonusesTransactionsUseCase {
 
     class Base(
         obtainAccessToken: ObtainAccessToken,
-        manageResources: ManageResources,
-        private val bonusesRepository: IBonusRepository
     ) : FetchBonusesTransactionsUseCase, AbstractTokenUseCase(obtainAccessToken) {
 
-        private val noData = manageResources.string(R.string.no_data)
-
         override suspend fun invoke(): Result<BonusesTransactionsState> = withToken { token ->
-            val transactions = buildList {
-                bonusesRepository.getTransactionsList(token).getOrThrow()?.map {
-                    add(
-                        BonusTransactionState(
-                            date = it.dateEvent?.parseToZDT()?.formatZdt("dd.MM.yyyy") ?: noData,
-                            amount = it.quantity?.toInt()?.toString() ?: noData,
-                            type = BonusTransactionType.BURNING
-                        )
-                    )
-                } ?: emptyList()
-            }
-            BonusesTransactionsState(transactions)
+            BonusesTransactionsState(emptyList())
         }
     }
 }
