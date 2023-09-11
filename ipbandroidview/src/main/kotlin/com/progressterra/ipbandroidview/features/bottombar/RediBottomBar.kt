9cfd95901851884030485caced62d575c747988f
velvet <berhram@proton.me>
2023-09-11 14:30:29 +0300

    fix: cart counter in bottom bar

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/features/bottombar/RediBottomBar.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/features/bottombar/RediBottomBar.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
@@ -56,7 +57,8 @@ fun RediBottomBar(
                         modifier = Modifier
                             .align(Alignment.TopEnd)
                             .clip(CircleShape)
-                            .background(IpbTheme.colors.primary.asBrush()),
+                            .background(IpbTheme.colors.primary.asBrush())
+                            .padding(1.dp),
                     ) {
                         BrushedText(
                             text = counter.toString(),
