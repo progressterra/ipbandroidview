063734c4d6c95ffd55db2207770e99f4a56a4f91
velvet <berhram@proton.me>
2023-09-14 13:50:27 +0300

    chore: unused interface

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/orderstatus/UseOrderStatusScreen.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/orderstatus/UseOrderStatusScreen.kt
@@ -1,12 +1,11 @@
 package com.progressterra.ipbandroidview.pages.orderstatus
 
-import com.progressterra.ipbandroidview.features.mainorreceipt.UseMainOrReceipt
 import com.progressterra.ipbandroidview.features.ordernumber.UseOrderNumber
 import com.progressterra.ipbandroidview.features.topbar.TopBarEvent
 import com.progressterra.ipbandroidview.features.topbar.UseTopBar
 import com.progressterra.ipbandroidview.shared.ui.button.ButtonEvent
 
-interface UseOrderStatusScreen : UseTopBar, UseMainOrReceipt, UseOrderNumber {
+interface UseOrderStatusScreen : UseTopBar, UseOrderNumber {
 
     class Empty : UseOrderStatusScreen {
 
