bbba01c712ae58a92687670e59295d5b269fe116
Daniíl Pechorin <berhram@protonmail.com>
2023-10-11 10:10:05 +0300

    fix: di

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/integration/IpbModule.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/integration/IpbModule.kt
@@ -6,20 +6,12 @@ import com.progressterra.ipbandroidview.pages.pagesModule
 import com.progressterra.ipbandroidview.processes.processesModule
 import com.progressterra.ipbandroidview.shared.sharedModule
 import com.progressterra.ipbandroidview.widgets.widgetsModule
-import org.koin.core.annotation.ComponentScan
-import org.koin.core.annotation.Module
 import org.koin.dsl.module
-import org.koin.ksp.generated.module
-
-@Module
-@ComponentScan
-class AnnotationModule
 
 
 @Suppress("unused")
 val ipbModule = module {
     includes(
-        AnnotationModule().module,
         iPBAndroidAPIModule,
         pagesModule,
         widgetsModule,
