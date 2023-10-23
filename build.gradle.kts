bc3c3cd56c21dcc2da21c465949f385b01575741
Daniíl Pechorin <berhram@protonmail.com>
2023-10-24 02:03:27 +0300

    empty test revert

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
 plugins {
     id("com.android.library") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.9.10" apply fals
+    id("org.jetbrains.kotlin.jvm") version "1.9.10" apply false
 }
 
 allprojects {
