6a940d1c6146d76552333791b400b4ed7fddcb1e
Daniíl Pechorin <berhram@protonmail.com>
2023-10-25 10:33:16 +0300

    fix: avatar fetching for current user

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/processes/dating/FetchDatingUserUseCase.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/processes/dating/FetchDatingUserUseCase.kt
@@ -4,6 +4,7 @@ import com.progressterra.ipbandroidapi.api.iamhere.ImhService
 import com.progressterra.ipbandroidview.entities.DatingUser
 import com.progressterra.ipbandroidview.entities.toDatingUser
 import com.progressterra.ipbandroidview.processes.ObtainAccessToken
+import com.progressterra.ipbandroidview.processes.user.FetchAvatarUseCase
 import com.progressterra.ipbandroidview.shared.AbstractTokenUseCase
 
 interface FetchDatingUserUseCase {
@@ -12,11 +13,13 @@ interface FetchDatingUserUseCase {
 
     class Base(
         obtainAccessToken: ObtainAccessToken,
-        private val service: ImhService
+        private val service: ImhService,
+        private val fetchAvatarUseCase: FetchAvatarUseCase
     ) : FetchDatingUserUseCase, AbstractTokenUseCase(obtainAccessToken) {
 
         override suspend fun invoke(): Result<DatingUser> = withToken { token ->
-            service.clientDataData(token).data?.toDatingUser()!!
+            val avatar = fetchAvatarUseCase().getOrThrow()
+            service.clientDataData(token).data?.toDatingUser()?.copy(image = avatar)!!
         }
     }
 }
