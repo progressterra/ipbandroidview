7bc24c4b17085edb457d54bd03ec13b181180d46
Daniíl Pechorin <berhram@protonmail.com>
2023-10-25 11:12:30 +0300

    fix: pfp picker

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/pfppicker/PfpPickerScreenState.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/pfppicker/PfpPickerScreenState.kt
@@ -7,6 +7,6 @@ import com.progressterra.ipbandroidview.shared.ui.statecolumn.StateColumnState
 
 data class PfpPickerScreenState(
     val pfpPicker: PfpPickerState = PfpPickerState(),
-    val choose: ButtonState = ButtonState(id = "choose"),
+    val choose: ButtonState = ButtonState(id = "choose", enabled = false),
     val skip: ButtonState = ButtonState(id = "skip")
 )
--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/pfppicker/PfpPickerScreenViewModel.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/pfppicker/PfpPickerScreenViewModel.kt
@@ -1,22 +1,41 @@
 package com.progressterra.ipbandroidview.pages.pfppicker
 
+import com.progressterra.ipbandroidview.R
 import com.progressterra.ipbandroidview.features.pfppicker.PfpPickerEvent
 import com.progressterra.ipbandroidview.features.topbar.TopBarEvent
+import com.progressterra.ipbandroidview.processes.user.PickPhotoUseCase
+import com.progressterra.ipbandroidview.processes.user.SaveAvatarUseCase
+import com.progressterra.ipbandroidview.processes.utils.MakeToastUseCase
 import com.progressterra.ipbandroidview.shared.mvi.AbstractNonInputViewModel
 import com.progressterra.ipbandroidview.shared.ui.button.ButtonEvent
 import com.progressterra.ipbandroidview.shared.ui.statecolumn.StateColumnEvent
 
-class PfpPickerScreenViewModel :
+class PfpPickerScreenViewModel(
+    private val pickPhotoUseCase: PickPhotoUseCase,
+    private val saveAvatarUseCase: SaveAvatarUseCase,
+    private val makeToastUseCase: MakeToastUseCase
+) :
     AbstractNonInputViewModel<PfpPickerScreenState, PfpPickerScreenEffect>(), UsePfpPickerScreen {
-
-    override fun refresh() {
-
-    }
 
     override fun createInitialState() = PfpPickerScreenState()
 
     override fun handle(event: PfpPickerEvent) {
-
+        onBackground {
+            pickPhotoUseCase().onSuccess { path ->
+                saveAvatarUseCase(path).onSuccess {
+                    emitState {
+                        it.copy(
+                            pfpPicker = it.pfpPicker.copy(url = path.toString()),
+                            choose = it.choose.copy(enabled = true)
+                        )
+                    }
+                    makeToastUseCase(R.string.success)
+                    refresh()
+                }.onFailure {
+                    makeToastUseCase(R.string.failure)
+                }
+            }
+        }
     }
 
     override fun handle(event: TopBarEvent) {
