ad0bd0712d0aa3f43116199b2e2b733a7740f8b4
Daniíl Pechorin <berhram@protonmail.com>
2023-11-09 00:58:08 +0300

    build: deps, ver

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,19 +6,19 @@ buildscript {
     }
     dependencies {
         classpath("com.google.gms:google-services:4.4.0")
-        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("com.android.tools.build:gradle:8.1.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
     }
 }
 
 plugins {
-    id("com.android.library") version "8.1.2" apply false
+    id("com.android.library") version "8.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("org.jetbrains.kotlin.jvm") version "1.9.10" apply false
 }
 
 allprojects {
-    version = "1.0.11"
+    version = "1.0.12"
     group = "com.progressterra.ipbandroidview"
     repositories {
         google()
