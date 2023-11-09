06dd1c6c33b6b32f073e568f72067b8f5cfdf821
Daniíl Pechorin <berhram@protonmail.com>
2023-11-09 15:30:36 +0300

    fix: messages on validation

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/processes/docs/DocumentValidationUseCase.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/processes/docs/DocumentValidationUseCase.kt
@@ -1,19 +1,14 @@
 package com.progressterra.ipbandroidview.processes.docs
 
 import com.progressterra.ipbandroidview.entities.Document
-import com.progressterra.ipbandroidview.processes.utils.MakeToastUseCase
-import com.progressterra.ipbandroidview.shared.AbstractLoggingUseCase
-import com.progressterra.ipbandroidview.shared.ManageResources
 
 interface DocumentValidationUseCase {
 
     suspend operator fun invoke(state: Document): Result<Unit>
 
-    class Base(makeToastUseCase: MakeToastUseCase, manageResources: ManageResources) : DocumentValidationUseCase, AbstractLoggingUseCase(
-        makeToastUseCase, manageResources
-    ) {
+    class Base : DocumentValidationUseCase {
 
-        override suspend fun invoke(state: Document): Result<Unit> = handle {
+        override suspend fun invoke(state: Document): Result<Unit> = runCatching {
             state.entries.forEach {
                 if (!it.valid()) {
                     throw Exception("Invalid!")
