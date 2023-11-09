83f03637ebe0fc35af86ac8fe09444a23bdbed72
Daniíl Pechorin <berhram@protonmail.com>
2023-11-10 02:05:55 +0300

    fix: error cathing

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/processes/dating/AvailableTargetsUseCase.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/processes/dating/AvailableTargetsUseCase.kt
@@ -3,10 +3,12 @@ package com.progressterra.ipbandroidview.processes.dating
 import com.progressterra.ipbandroidapi.api.iamhere.ImhService
 import com.progressterra.ipbandroidapi.api.iamhere.models.FilterAndSort
 import com.progressterra.ipbandroidapi.api.iamhere.models.SortData
+import com.progressterra.ipbandroidapi.api.iamhere.models.StatusResult
 import com.progressterra.ipbandroidapi.api.iamhere.models.TypeVariantSort
 import com.progressterra.ipbandroidview.entities.DatingTarget
 import com.progressterra.ipbandroidview.entities.toDatingTarget
 import com.progressterra.ipbandroidview.processes.ObtainAccessToken
+import com.progressterra.ipbandroidview.processes.ToastedException
 import com.progressterra.ipbandroidview.processes.utils.MakeToastUseCase
 import com.progressterra.ipbandroidview.shared.AbstractTokenUseCase
 import com.progressterra.ipbandroidview.shared.ManageResources
@@ -19,22 +21,29 @@ interface AvailableTargetsUseCase {
         obtainAccessToken: ObtainAccessToken,
         private val service: ImhService, makeToastUseCase: MakeToastUseCase,
         manageResources: ManageResources
-    ) : AvailableTargetsUseCase, AbstractTokenUseCase(obtainAccessToken, makeToastUseCase,
+    ) : AvailableTargetsUseCase, AbstractTokenUseCase(
+        obtainAccessToken, makeToastUseCase,
         manageResources
     ) {
 
         override suspend fun invoke(): Result<List<DatingTarget>> = withToken { token ->
-            service.targetList(token,
+            val result = service.targetList(
+                token,
                 body = FilterAndSort(
-                listFields = emptyList(),
-                sort = SortData(
-                    fieldName = "dateAdded",
-                    variantSort = TypeVariantSort.DESC
-                ),
-                searchData = "",
-                skip = 0,
-                take = 100)
-            ).dataList?.map { it.toDatingTarget() } ?: emptyList()
+                    listFields = emptyList(),
+                    sort = SortData(
+                        fieldName = "dateAdded",
+                        variantSort = TypeVariantSort.DESC
+                    ),
+                    searchData = "",
+                    skip = 0,
+                    take = 100
+                )
+            )
+            if (result.result?.status != StatusResult.SUCCESS) {
+                throw ToastedException(result.result?.message ?: "")
+            }
+            result.dataList?.map { it.toDatingTarget() } ?: emptyList()
         }
     }
 }
