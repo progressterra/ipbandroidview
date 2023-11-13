66fc373a500a194b029cbb5a9f3b30b7ce2aa656
Daniíl Pechorin <berhram@protonmail.com>
2023-11-14 01:13:41 +0300

    feat: advanced logging

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/shared/Log.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/shared/Log.kt
@@ -4,5 +4,13 @@ import android.util.Log
 import com.progressterra.ipbandroidview.IpbAndroidViewSettings
 
 fun log(tag: String, msg: String) {
-    if (IpbAndroidViewSettings.DEBUG) Log.d(tag, msg)
+    if (IpbAndroidViewSettings.DEBUG) {
+        val maxMsgLength = 2000
+        if (msg.length > maxMsgLength) {
+            Log.d(tag, msg.substring(0, maxMsgLength))
+            log(tag, msg.substring(maxMsgLength))
+        } else {
+            Log.d(tag, msg)
+        }
+    }
 }
