8bedf88de956f2e608afe30a39989143a3f8c713
Daniíl Pechorin <berhram@protonmail.com>
2023-11-14 03:38:52 +0300

    fix: work watch worker creation

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/integration/WorkWatchWorker.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/integration/WorkWatchWorker.kt
@@ -3,23 +3,34 @@ package com.progressterra.ipbandroidview.integration
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.google.android.gms.location.LocationServices
+import com.progressterra.ipbandroidapi.IpbAndroidApiSettings
+import com.progressterra.ipbandroidapi.api.auth.AuthService
 import com.progressterra.ipbandroidapi.api.workwatch.WorkWatchService
 import com.progressterra.ipbandroidapi.api.workwatch.models.RGTrackingEntity
+import com.progressterra.ipbandroidapi.core.NetworkService
 import com.progressterra.ipbandroidview.processes.ObtainAccessToken
 import com.progressterra.ipbandroidview.processes.location.ProvideLocationUseCase
 
 class WorkWatchWorker(
-    context: Context,
-    parameters: WorkerParameters,
-    private val obtainAccessToken: ObtainAccessToken,
-    private val locationUseCase: ProvideLocationUseCase,
-    private val workWatchService: WorkWatchService
+    private val context: Context,
+    parameters: WorkerParameters
 ) : CoroutineWorker(context, parameters) {
 
     override suspend fun doWork(): Result {
         val result = runCatching {
-            val token = obtainAccessToken().getOrThrow()
-            val location = locationUseCase().getOrThrow()
+            val networkService = NetworkService.Base()
+            val authService = networkService.createService(
+                AuthService::class.java, IpbAndroidApiSettings.AUTH_URL
+            )
+            val workWatchService = networkService.createService(
+                WorkWatchService::class.java, IpbAndroidApiSettings.WORK_WATCH_URL
+            )
+            val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+            val provideLocationUseCase = ProvideLocationUseCase.Base(fusedLocationProviderClient)
+            val obtainAccessTokenUseCase = ObtainAccessToken.Base(authService, provideLocationUseCase)
+            val token = obtainAccessTokenUseCase().getOrThrow()
+            val location = provideLocationUseCase().getOrThrow()
             workWatchService.clientAreaTracking(
                 token = token,
                 body = RGTrackingEntity(
--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/workwatch/WorkWatchScreenState.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/pages/workwatch/WorkWatchScreenState.kt
@@ -6,5 +6,5 @@ import com.progressterra.ipbandroidview.shared.ui.button.ButtonState
 @Immutable
 data class WorkWatchScreenState(
     val ask: ButtonState = ButtonState(id = "ask"),
-    val enable: ButtonState = ButtonState(id = "enable")
+    val enable: ButtonState = ButtonState(id = "enable", enabled = false)
 )
