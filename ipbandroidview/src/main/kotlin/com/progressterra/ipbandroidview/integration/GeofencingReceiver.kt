58f49b9ebfef9daa6d5f08fa26d0fc3747764cc1
Daniíl Pechorin <berhram@protonmail.com>
2023-12-01 12:26:50 +0300

    feat: notifications

--- ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/integration/GeofencingReceiver.kt
+++ ipbandroidview/src/main/kotlin/com/progressterra/ipbandroidview/integration/GeofencingReceiver.kt
@@ -5,25 +5,37 @@ import android.content.Context
 import android.content.Intent
 import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofencingEvent
+import com.progressterra.ipbandroidview.processes.utils.MakeNotificationUseCase
 import com.progressterra.ipbandroidview.shared.log
 
 abstract class GeofencingReceiver : BroadcastReceiver() {
 
+    abstract val iconId: Int
+
+    abstract val channelId: String
+
+    abstract val channelName: String
+
+    abstract val activity: Class<*>
+
     override fun onReceive(context: Context, intent: Intent) {
-        val geofencingEvent = GeofencingEvent.fromIntent(intent)
-        if (geofencingEvent == null) {
-            log("Geofencing", "event is null")
-            return
-        }
+        val geofencingEvent = GeofencingEvent.fromIntent(intent) ?: return
         if (geofencingEvent.hasError()) {
             log("Geofencing", "error: ${geofencingEvent.errorCode}")
             return
         }
         val geofenceTransition = geofencingEvent.geofenceTransition
+        val makeNotificationUseCase = MakeNotificationUseCase.Base(
+            context = context,
+            iconId = iconId,
+            channelId = channelId,
+            channelName = channelName,
+            activity = activity
+        )
         if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
-            log("Geofencing", "enter ${geofencingEvent.triggeringGeofences?.map { it.requestId }}")
+            makeNotificationUseCase("Geofencing", "Entering ${geofencingEvent.triggeringGeofences?.map { it.requestId }}")
         } else if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
-            log("Geofencing", "exit ${geofencingEvent.triggeringGeofences?.map { it.requestId }}")
+            makeNotificationUseCase("Geofencing", "Exiting ${geofencingEvent.triggeringGeofences?.map { it.requestId }}")
         }
     }
 }
